617e5d002ef63045a32b7085f8d588e8e3cf049d
Saulius Grigaliunas <saulius@ninja.lt>
2012-06-07 10:20:20 +0100

    Remove sidekiq, render error and 404s in json

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,37 @@
 class ApplicationController < ActionController::Base
   respond_to :json, :html
+
+  unless Rails.application.config.consider_all_requests_local
+    rescue_from Exception, with: :render_error
+    rescue_from Mongoid::Errors::DocumentNotFound, with: :render_not_found
+    rescue_from ActionController::RoutingError, with: :render_not_found
+    rescue_from ActionController::UnknownController, with: :render_not_found
+    rescue_from ActionController::UnknownAction, with: :render_not_found
+  end
+
+  def render_not_found
+    respond_to do |format|
+      format.html {
+        render text: "404 - Not Found"
+      }
+      format.json {
+        render json: { error: "404 - Not Found" }
+      }
+    end
+  end
+
+  def render_error(exception)
+    notify_airbrake(exception)
+    logger.error exception
+    logger.error exception.backtrace
+
+    respond_to do |format|
+      format.html {
+        render text: "500 - Internal Server Error: #{exception.message}"
+      }
+      format.json {
+        render json: { error: "500 - Internal Server Error: #{exception.message}" }
+      }
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
-# require 'sidekiq/web'
 require 'api_constraints'
 
 UKTradeTariff::Application.routes.draw do
-  # mount Sidekiq::Web => '/sidekiq'
-
   namespace :api, defaults: {format: 'json'}, path: "/" do
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :sections, only: [:index, :show], constraints: { id: /\d{1,2}/ }
