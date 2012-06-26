2a356fb6d2deed2fc248dae2ed81d8abdaafe244
Matthew Ford <matt@bitzesty.com>
2012-06-26 14:50:17 +0100

    use AR again

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
 require File.expand_path('../boot', __FILE__)
 
-require "action_controller/railtie"
+require "rails/all"
 require "sprockets/railtie"
 # require 'tire/rails/logger' if Rails.env.development?
 
