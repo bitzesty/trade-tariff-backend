ce82e5bd1584c9a117c01e1f8f5158c1e2f8de9d
Saulius Grigaliunas <saulius@ninja.lt>
2012-06-27 12:42:24 +0100

    Add missing commas

--- app/models/additional_code_type.rb
+++ app/models/additional_code_type.rb
@@ -1,9 +1,8 @@
 class AdditionalCodeType < ActiveRecord::Base
   APPLICATION_CODES = {
-    0 => "Export refund nomencalture"
-    1 => "Additional Codes"
-    3 => "Meursing addition codes"
+    0 => "Export refund nomencalture",
+    1 => "Additional Codes",
+    3 => "Meursing addition codes",
     4 => "Eport refund for processed agricultural goods"
   }
-
 end
