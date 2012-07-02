087d3573bfb2e60cee0db3a9c818af02931d5ba2
Saulius Grigaliunas <saulius@ninja.lt>
2012-07-02 16:12:42 +0100

    Specify classes properly

--- app/models/additional_code_description.rb
+++ app/models/additional_code_description.rb
@@ -3,6 +3,8 @@ class AdditionalCodeDescription < ActiveRecord::Base
 
   belongs_to :additional_code_description_period, foreign_key: :additional_code_description_period_sid
   belongs_to :language
-  belongs_to :code_type, foreign_key: :additional_code_type_id
-  belongs_to :code, foreign_key: :additional_code_sid
+  belongs_to :code_type, foreign_key: :additional_code_type_id,
+                         class_name: 'AdditionalCodeType'
+  belongs_to :code, foreign_key: :additional_code_sid,
+                    class_name: 'AdditionalCode'
 end
--- app/models/additional_code_description_period.rb
+++ app/models/additional_code_description_period.rb
@@ -3,6 +3,8 @@ class AdditionalCodeDescriptionPeriod < ActiveRecord::Base
 
   has_one :description, foreign_key: :additional_code_description_period_sid,
                         class_name: 'AdditionalCodeDescription'
-  belongs_to :code, foreign_key: :additional_code_sid
-  belongs_to :code_type, foreign_key: :additional_code_type_id
+  belongs_to :code, foreign_key: :additional_code_sid,
+                    class_name: 'AdditionalCode'
+  belongs_to :code_type, foreign_key: :additional_code_type_id,
+                         class_name: 'AdditionalCodeType'
 end
