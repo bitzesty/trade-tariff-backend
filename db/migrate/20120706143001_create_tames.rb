b30356c09c35df944c209ce59181a0f01f651c7d
Matthew Ford <matt@bitzesty.com>
2012-07-06 17:56:03 +0100

    tame migration (might need to remove id)

---
+++ db/migrate/20120706143001_create_tames.rb
@@ -0,0 +1,42 @@
+class CreateTames < ActiveRecord::Migration
+  def change
+    create_table :tame do |t|
+      t.datetime :fe_tsmp
+      t.string :msgrp_code
+      t.string :msr_type
+      t.string :tty_code
+      t.string :tar_msr_no
+      t.datetime :le_tsmp
+      t.boolean :current_ind
+      t.decimal :adval_rate, :precision => 3, :scale => 3
+      t.decimal :alch_sgth, :precision => 3, :scale => 2
+      t.datetime :audit_tsmp
+      t.string :cap_ai_stmt
+      t.decimal :cap_max_pct, :precision => 3, :scale => 3
+      t.string :cmdty_msr_xhdg
+      t.string :comp_mthd
+      t.string :cpc_wvr_phb
+      t.string :ec_msr_set
+      t.string :mip_band_exch
+      t.string :mip_rate_exch
+      t.string :mip_uoq_code
+      t.string :nba_id
+      t.string :null_tri_rqd
+      t.string :qta_code_uk
+      t.string :qta_elig_useLstrubg
+      t.string :qta_exch_rate
+      t.string :qta_no
+      t.string :qta_uoq_code
+      t.text :rfa
+      t.string :rfs_code_1
+      t.string :rfs_code_2
+      t.string :rfs_code_3
+      t.string :rfs_code_4
+      t.string :rfs_code_5
+      t.string :tdr_spr_sur
+      t.boolean :exports_use_ind
+
+      t.timestamps
+    end
+  end
+end
