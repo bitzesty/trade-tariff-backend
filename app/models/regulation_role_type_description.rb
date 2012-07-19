3d7b16a677708f68f494e514609301f69fb149a8
Saulius Grigaliunas <saulius@ninja.lt>
2012-07-19 09:14:33 +0100

    Comment out ActiveRecord associations. Define Chief module on CountryCode.

--- app/models/regulation_role_type_description.rb
+++ app/models/regulation_role_type_description.rb
@@ -1,8 +1,8 @@
 class RegulationRoleTypeDescription < Sequel::Model
   set_primary_key  :regulation_role_type_id
 
-  belongs_to :regulation_role_type
-  belongs_to :language
+  # belongs_to :regulation_role_type
+  # belongs_to :language
 end
 
 # == Schema Information
