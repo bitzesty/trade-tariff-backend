a1b3216d1b3692515ef0138a439026d2cb26263f
Saulius Grigaliunas <saulius@ninja.lt>
2012-07-20 14:42:36 +0100

    Using db and migrations.

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
+RAILS_ENV=test bundle exec rake db:setup
+RAILS_ENV=test bundle exec rake db:migrate
 RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
 exit $RESULT
