18e575c79b09fc0db453e267c12f8e7428610ac9
Saulius Grigaliunas <saulius@ninja.lt>
2012-07-24 09:40:44 +0100

    This method will not be used. If not used in TimeMachine.at date bounded models will fetch relative associations by default.

--- lib/time_machine.rb
+++ lib/time_machine.rb
@@ -10,23 +10,13 @@ module TimeMachine
       datetime = DateTime.parse(datetime) if datetime.is_a?(String)
 
       previous = Thread.current[THREAD_DATETIME_KEY],
-      previous_strategy = Thread.current[THREAD_STRATEGY_KEY]
       raise ArgumentError, "requires a block" unless block_given?
       Thread.current[THREAD_DATETIME_KEY] = datetime
       Thread.current[THREAD_STRATEGY_KEY] = :absolute
       yield
     ensure
       Thread.current[THREAD_DATETIME_KEY] = previous
-      Thread.current[THREAD_STRATEGY_KEY] = previous_strategy
-  end
-
-  def self.relevant(&block)
-      previous_strategy = Thread.current[THREAD_STRATEGY_KEY]
-      raise ArgumentError, "requires a block" unless block_given?
-      Thread.current[THREAD_STRATEGY_KEY] = :relevant
-      yield
-    ensure
-      Thread.current[THREAD_STRATEGY_KEY] = previous_strategy
+      Thread.current[THREAD_STRATEGY_KEY] = nil
   end
 
   def self.now(&block)
