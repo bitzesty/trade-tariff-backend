51778b533d4d9fded7b2539846e9c91f1244a619
James Abley <james.abley@digital.cabinet-office.gov.uk>
2012-07-25 12:10:13 +0100

    Document API versioning question

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ require 'api_constraints'
 
 UKTradeTariff::Application.routes.draw do
   namespace :api, defaults: {format: 'json'}, path: "/" do
+    # How (or even if) API versioning will be implemented is still an open question. We can defer
+    # the choice until we need to expose the API to clients which we don't control.
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
       resources :sections, only: [:index, :show], constraints: { id: /\d{1,2}/ }
       resources :chapters, only: [:show], constraints: { id: /\d{2}/ }
--- lib/api_constraints.rb
+++ lib/api_constraints.rb
@@ -5,6 +5,10 @@ class ApiConstraints
   end
 
   def matches?(req)
+    # API versioning isn't something that we've made a decision on yet, so this might be subject
+    # to change.
+    # Currently the only client of the API is TradeTariffWebApp and we are free to evolve both the
+    # server and the client in lockstep.
     @default || req.headers['Accept'].include?("application/vnd.uktt.v#{@version}")
   end
 end
