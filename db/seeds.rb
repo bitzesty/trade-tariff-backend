6ab2c25b116f6923ba5fc6b970b2e1b1d00fbdd7
Saulius Grigaliunas <saulius@ninja.lt>
2012-07-31 14:27:57 +0300

    Disable api key creation. No such model for now.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,3 @@
-a = ApiKey.create(app_id: "tariff-web")
-a.access_token = "86c337686edc492184c5e9869c27a0b1"
-a.save
+# a = ApiKey.create(app_id: "tariff-web")
+# a.access_token = "86c337686edc492184c5e9869c27a0b1"
+# a.save
