4acb768f1286ab27d6c3d882992abb3c466ef0d4
Saulius Grigaliunas <saulius@ninja.lt>
2012-08-01 12:47:20 +0300

    Chapter and section association should be time bound.

--- app/models/section.rb
+++ app/models/section.rb
@@ -1,8 +1,12 @@
 class Section < Sequel::Model
   plugin :json_serializer
 
-  many_to_many :chapters, right_key: :goods_nomenclature_sid,
-                          join_table: :chapters_sections
+  many_to_many :chapters, dataset: -> {
+    Chapter.join_table(:inner, :chapters_sections, chapters_sections__goods_nomenclature_sid: :goods_nomenclatures__goods_nomenclature_sid)
+           .join_table(:inner, :sections, chapters_sections__section_id: :sections__id)
+           .with_actual(Chapter)
+           .where(sections__id: id)
+  }
 
   def to_param
     position
