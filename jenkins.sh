d669fb79a9eb4dff1545ba8c32ee5caddf10e333
Matthew Ford <matt@bitzesty.com>
2012-08-02 17:23:24 +0100

    add --trace

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,8 @@
 #!/bin/bash -x
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
-RAILS_ENV=test bundle exec rake db:drop
-RAILS_ENV=test bundle exec rake db:create
-RAILS_ENV=test bundle exec rake db:schema:load
+RAILS_ENV=test bundle exec rake db:drop --trace
+RAILS_ENV=test bundle exec rake db:create --trace
+RAILS_ENV=test bundle exec rake db:schema:load --trace
 RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
 exit $RESULT
