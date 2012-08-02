d03877289c338edf68b2d4ddc55ef6e6d40b9e9e
Matthew Ford <matt@bitzesty.com>
2012-08-02 17:39:45 +0100

    adding sleep 5

--- jenkins.sh
+++ jenkins.sh
@@ -1,8 +1,12 @@
 #!/bin/bash -x
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
-RAILS_ENV=test bundle exec rake db:drop --trace
+
 RAILS_ENV=test bundle exec rake db:create --trace
+sleep 5
+
 RAILS_ENV=test bundle exec rake db:schema:load --trace
+
+sleep 5
 RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
 exit $RESULT
