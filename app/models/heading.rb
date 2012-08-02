8f36a9c98bc329e6cbc5bc1b48e03f8b9d924044
Saulius Grigaliunas <saulius@ninja.lt>
2012-08-02 08:47:21 +0300

    Heading #declarable check should take into account heading's validity period.

--- app/models/heading.rb
+++ app/models/heading.rb
@@ -73,7 +73,7 @@ class Heading < GoodsNomenclature
   end
 
   def declarable
-    GoodsNomenclature.where("goods_nomenclature_item_id LIKE ?", "#{short_code}______").count == 1
+    actual(GoodsNomenclature).where("goods_nomenclature_item_id LIKE ?", "#{short_code}______").count == 1
   end
   alias :declarable? :declarable
 
