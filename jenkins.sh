f857d7a619786f2bebd2160f9fffb8307685027b
Matthew Ford <matt@bitzesty.com>
2012-08-02 18:28:47 +0100

    last try before heading home

--- jenkins.sh
+++ jenkins.sh
@@ -1,15 +1,9 @@
 #!/bin/bash -x
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
 
-RAILS_ENV=test bundle exec rake db:drop
-sleep 10
+bundle exec rake db:force_close_open_connections
+bundle exec rake db:reset
 
-RAILS_ENV=test bundle exec rake db:create
-sleep 10
-
-RAILS_ENV=test bundle exec rake db:schema:load --trace
-
-sleep 10
 RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
 exit $RESULT
