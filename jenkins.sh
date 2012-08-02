62823a0f3fa3d7d106c10d01b71da6e171d86b05
Matthew Ford <matt@bitzesty.com>
2012-08-02 16:48:11 +0100

    footnote xls and drop db in test before a schema load

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -x
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
+RAILS_ENV=test bundle exec rake db:drop
 RAILS_ENV=test bundle exec rake db:schema:load
 RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
