b236fb32a2f012f6e44c1f69efbc0900b569cf0a
Matthew Ford <matt@bitzesty.com>
2012-08-02 16:59:27 +0100

    forgot to create the db after drop

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
 RAILS_ENV=test bundle exec rake db:drop
+RAILS_ENV=test bundle exec rake db:create
 RAILS_ENV=test bundle exec rake db:schema:load
 RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
