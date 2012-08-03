d53c2d5fc78540ab7aa1a7290896462bf8d09f7b
Saulius Grigaliunas <saulius@ninja.lt>
2012-08-03 09:38:28 +0300

    Missing db:migrate in ci script.

--- jenkins.sh
+++ jenkins.sh
@@ -4,6 +4,7 @@ bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
 bundle exec rake db:force_close_open_connections
 bundle exec rake db:reset
 
+RAILS_ENV=test bundle exec rake db:migrate
 RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
 exit $RESULT
