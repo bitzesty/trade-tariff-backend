162a938002d27a1fe197cf8a45319504c4ea3036
Matthew Ford <matt@bitzesty.com>
2012-08-06 23:24:38 +0100

    Adding primary keys to Mfcm Tame Tamf and default ordering

--- app/models/chief/tame.rb
+++ app/models/chief/tame.rb
@@ -1,5 +1,13 @@
 module Chief
   class Tame < Sequel::Model(:chief_tame)
-  # set_primary_key TODO
+    set_primary_key [:msrgp_code, :msr_type, :tty_code, :fe_tsmp]
+     
+    set_dataset order(:msrgp_code.asc).
+                order_more(:msr_type.asc).
+                order_more(:tty_code.asc).
+                order_more(:fe_tsmp.desc)
+
   end
 end
+
+
--- app/models/chief/tamf.rb
+++ app/models/chief/tamf.rb
@@ -1,5 +1,10 @@
 module Chief
   class Tamf < Sequel::Model(:chief_tamf)
-  # set_primary_key TODO
+    set_primary_key [:msrgp_code, :msr_type, :tty_code, :cngp_code, :cntry_orig, :fe_tsmp]
+    
+    set_dataset order(:msrgp_code.asc).
+              order_more(:msr_type.asc).
+              order_more(:tty_code.asc).
+              order_more(:fe_tsmp.desc)
   end
 end
