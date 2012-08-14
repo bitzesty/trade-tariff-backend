7ca9a411468677f35e1424025b9d2612e3763f30
Matthew Ford <matt@bitzesty.com>
2012-08-14 12:26:13 +0000

    start_date validator for ensuring the start date is not after the end date

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,6 +1,16 @@
 class Language < Sequel::Model
   set_primary_key  :language_id
 
+  ######### Conformance validations 130
+  # LA2 - TODO
+  def validate
+    super
+    # LA1
+    validates_unique(:language_id)
+    # LA3
+    validates_start_date
+  end
+  
   # has_many :language_descriptions
   # has_many :additional_code_descriptions
   # has_many :additional_code_type_descriptions
--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -2,3 +2,6 @@ require 'sequel/extensions/pagination_compat'
 
 Sequel.extension     :pagination_compat
 Sequel::Model.plugin :take
+Sequel::Model.plugin :validation_helpers
+Sequel::Model.plugin :tariff_validation_helpers
+
