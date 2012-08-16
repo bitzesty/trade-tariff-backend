3660cd929684a1c9095d31fe0d34c6d61dee858a
Matthew Ford <matt@bitzesty.com>
2012-08-16 16:05:50 +0100

    measures validations need to update the spec and use correct syntax

--- config/initializers/sequel.rb
+++ config/initializers/sequel.rb
@@ -1,10 +1,9 @@
 require 'sequel/extensions/pagination_compat'
 
-Sequel::Model.plugin :take
-
 # Defaults for sensible migrations
 Sequel::MySQL.default_engine = 'InnoDB'
 Sequel::MySQL.default_charset = 'utf8'
 
+Sequel::Model.plugin :take
 Sequel::Model.plugin :validation_helpers
 Sequel::Model.plugin :tariff_validation_helpers
---
+++ spec/unit/sequel/extensions/validations_helper_spec.rb
@@ -0,0 +1,9 @@
+require 'spec_helper'
+
+describe Sequel::Plugins::TariffValidationHelpers do
+  describe 'validates_start_date' do
+    it 'checks that validity_start_date is less than or equal to validity_endx_date' 
+
+    it 'allows a blank end date' 
+  end
+end
