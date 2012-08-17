39649bbf59bb62085d26d7314f2cd0ed2bce893b
Saulius Grigaliunas <saulius@ninja.lt>
2012-08-17 14:39:54 +0300

    Fix task names, add sync namespace.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,9 +1,9 @@
 set :output, { error: 'log/cron.error.log', standard: 'log/cron.log'}
 
 every 1.hour do
-  rake "tariff:download"
+  rake "tariff:sync:download"
 end
 
 every 1.day, at: '5am' do
-  rake "tariff:sync"
+  rake "tariff:sync:apply"
 end
