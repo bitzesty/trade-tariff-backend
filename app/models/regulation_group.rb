87b9e74b2ebd8a0e067373ca1a23fcc36197b0e4
Matthew Ford <matt@bitzesty.com>
2012-08-17 12:07:37 +0100

    regulation group validations

--- app/models/regulation_group.rb
+++ app/models/regulation_group.rb
@@ -3,6 +3,15 @@ class RegulationGroup < Sequel::Model
 
   # has_many :base_regulations
   # has_one  :regulation_group_description, foreign_key: :regulation_group_id
+
+  # RG2 - TODO
+  def validate
+    super
+    # RG1
+    validates_unique(:regulation_group_id)
+    # RG3
+    validates_start_date
+  end
 end
 
 
