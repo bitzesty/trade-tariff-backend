89a96cdec293596564993511887b06bb28b35e80
Saulius Grigaliunas <saulius@ninja.lt>
2012-09-17 15:59:18 +0300

    Load schema on jenkins.

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
 RAILS_ENV=test bundle exec rake db:force_close_open_connections
 RAILS_ENV=test bundle exec rake db:reset
 
-RAILS_ENV=test bundle exec rake db:migrate
+RAILS_ENV=test bundle exec rake db:schema_load
 RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
 exit $RESULT
