b95bbb65d2b97cc644468fb6a9a6732344b20bc0
Saulius Grigaliunas <saulius@ninja.lt>
2012-09-17 16:04:10 +0300

    Use general task to prepare db for testing.

--- jenkins.sh
+++ jenkins.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -x
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
 
-RAILS_ENV=test bundle exec rake db:force_close_open_connections
-RAILS_ENV=test bundle exec rake db:reset
-
-RAILS_ENV=test bundle exec rake db:schema_load
+RAILS_ENV=test bundle exec rake db:test:prepare
 RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
 exit $RESULT
