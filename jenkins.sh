fce5da0e7396649de3cc64d56dad5bb59404df7e
James Abley <james.abley@gmail.com>
2012-09-18 08:52:47 +0100

    Bring the jenkins script in line with others.
    Build is currently failing due to db being in an inconsistent state.

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
-
+RAILS_ENV=test bundle exec rake db:setup
 RAILS_ENV=test bundle exec rake db:migrate
 RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
