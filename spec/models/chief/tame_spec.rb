c8352aa53a3262278ac0f3e4d6e78d1fa9c6feaf
Saulius Grigaliunas <saulius@ninja.lt>
2012-09-24 16:44:36 +0300

    Update spec in accordance to tame<->tamf relation changes.

--- spec/models/chief/tame_spec.rb
+++ spec/models/chief/tame_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe Chief::Tame do
-  let(:common_tame_attributes) { attributes_for(:tame).slice(:msrgp_code, :msr_type, :tty_code, :fe_tsmp) }
+  let(:common_tame_attributes) { attributes_for(:tame).slice(:fe_tsmp, :msrgp_code, :msr_type, :tty_code, :fe_tsmp) }
 
   describe 'associations' do
     describe 'tamfs' do
@@ -18,8 +18,8 @@ describe Chief::Tame do
         let!(:tamf1) { create :tamf, common_tame_attributes.merge(fe_tsmp: Date.today.ago(20.years)) }
 
         it 'latest relevant tamf record is chosen' do
-          tame.tamfs.should include tamf
-          tame.tamfs.should include tamf1
+          tame.tamfs.should     include tamf
+          tame.tamfs.should_not include tamf1
         end
       end
     end
