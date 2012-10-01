22da6fdb3fa634150f3d6306066c346e64d49868
Matthew Ford <matt@bitzesty.com>
2012-10-01 17:53:03 +0100

    de1, de2

--- app/models/duty_expression.rb
+++ app/models/duty_expression.rb
@@ -7,6 +7,18 @@ class DutyExpression < Sequel::Model
 
   delegate :description, to: :duty_expression_description
 
+  ######### Conformance validations 235
+  def validate
+    super
+    # DE1
+    validates_unique([:duty_expression_id])
+    # DE2
+    validates_start_date
+    # DE3
+    # DE4
+  end
+
+
   # has_many :measure_components, foreign_key: :duty_expression_id
   # has_many :measures, through: :measure_components
   # has_many :measure_condition_components, foreign_key: :duty_expression_id
