85ffe88f165b85f2af5e4be606b1b3fcf873d4d1
Saulius Grigaliunas <saulius@ninja.lt>
2012-10-09 12:48:32 +0300

    Convert MeasurementUnit ASX to ASV.
    Also, fix other migrations.

--- db/migrate/20121008175409_add_abbreviation_to_duty_expression.rb
+++ db/migrate/20121008175409_add_abbreviation_to_duty_expression.rb
@@ -1,7 +1,13 @@
 Sequel.migration do
-  change do
-    alter_table :duty_expression_description do
+  up do
+    alter_table :duty_expression_descriptions do
       add_column :abbreviation, String, size: 30
+    end
+  end
+
+  down do
+    alter_table :duty_expression_descriptions do
+      drop_column :abbreviation
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1531,6 +1531,7 @@ Sequel.migration do
     self[:schema_migrations].insert(:filename => "20121004172558_extend_tariff_updates_size.rb")
     self[:schema_migrations].insert(:filename => "20121008175409_add_abbreviation_to_duty_expression.rb")
     self[:schema_migrations].insert(:filename => "20121008181507_manual_add_abbreviations.rb")
+    self[:schema_migrations].insert(:filename => "20121009092643_change_asx_to_asv.rb")
 
     create_table(:search_references) do
       primary_key :id, :type=>"int(11)"
