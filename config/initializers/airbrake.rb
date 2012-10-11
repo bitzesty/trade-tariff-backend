36ee9d327176db721a3726e7bb029f13b952cbd4
Saulius Grigaliunas <saulius@ninja.lt>
2012-10-11 12:14:20 +0300

    Temp remove errbit.

--- config/initializers/airbrake.rb
+++ config/initializers/airbrake.rb
@@ -1,6 +1,6 @@
-Airbrake.configure do |config|
-  config.api_key     = 'e69ba9033db17057825a09a0f2532cff'
-  config.host        = 'errbit.apps.bitzesty.com'
-  config.port        = 80
-  config.secure      = config.port == 443
-end
+# Airbrake.configure do |config|
+#   config.api_key     = 'e69ba9033db17057825a09a0f2532cff'
+#   config.host        = 'errbit.apps.bitzesty.com'
+#   config.port        = 80
+#   config.secure      = config.port == 443
+# end
