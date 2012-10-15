b73be5fd647598e30ebb6992afb4a37b6d5ef14e
Matthew Ford <matt@bitzesty.com>
2012-10-15 14:56:35 +0000

    use curl in insecure mode, for fetching files

--- lib/tariff_synchronizer/file_service.rb
+++ lib/tariff_synchronizer/file_service.rb
@@ -38,6 +38,7 @@ module TariffSynchronizer
       begin
         crawler = Curl::Easy.new(url)
         crawler.ssl_verify_peer = false
+        crawler.ssl_verify_host = false
         crawler.http_auth_types = :basic
         crawler.username = TariffSynchronizer.username
         crawler.password = TariffSynchronizer.password
