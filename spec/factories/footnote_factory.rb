6f85b8286bce53020b6d41df8a9be5a60fa05752
Saulius Grigaliunas <saulius@ninja.lt>
2012-10-24 14:39:04 +0300

    Resolve merge conflict.

--- spec/factories/footnote_factory.rb
+++ spec/factories/footnote_factory.rb
@@ -67,4 +67,10 @@ FactoryGirl.define do
     footnote_id                     { Forgery(:basic).text(exactly: 3) }
     footnote_type_id                { Forgery(:basic).text(exactly: 2) }
   end
+
+  factory :footnote_type do
+    footnote_type_id { Forgery(:basic).text(exactly: 2) }
+    validity_start_date { Date.today.ago(2.years) }
+    validity_end_date   { nil }
+  end
 end
