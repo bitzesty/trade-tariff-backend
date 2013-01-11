1a447e203a6e70a996d63e63e7e950b6dce96c53
Saulius Grigaliunas <saulius@ninja.lt>
2013-01-11 11:52:58 +0200

    Specs for TariffValidationsHelper.

---
+++ spec/unit/sequel/spec_helper.rb
@@ -0,0 +1,13 @@
+class << Sequel::Model
+  attr_writer :db_schema
+  alias orig_columns columns
+  def columns(*cols)
+    return super if cols.empty?
+    define_method(:columns){cols}
+    @dataset.instance_variable_set(:@columns, cols) if @dataset
+    def_column_accessor(*cols)
+    @columns = cols
+    @db_schema = {}
+    cols.each{|c| @db_schema[c] = {}}
+  end
+end
