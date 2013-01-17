3afda1a6b1849fc91ee690feb35152acb77a41f4
Saulius Grigaliunas <saulius@ninja.lt>
2013-01-17 16:45:00 +0200

    Refactor associations on QuotaOrderNumber.

--- app/models/quota_order_number.rb
+++ app/models/quota_order_number.rb
@@ -3,22 +3,10 @@ class QuotaOrderNumber < Sequel::Model
 
   set_primary_key  :quota_order_number_sid
 
-  one_to_one :quota_definition, eager_loader_key: :quota_order_number_sid, dataset: -> {
-    actual(QuotaDefinition).where(quota_order_number_id: quota_order_number_id)
-  }, eager_loader: (proc do |eo|
-    eo[:rows].each{|measure| measure.associations[:quota_order_number] = nil}
-
-    id_map = eo[:id_map]
-
-    QuotaDefinition.actual
-                   .where(quota_order_number_sid: id_map.keys).all do |quota_definition|
-      if measures = id_map[quota_definition.quota_order_number_id]
-        measures.each do |measure|
-          measure.associations[:quota_definition] = quota_defitinion
-        end
-      end
-    end
-  end)
+  one_to_one :quota_definition, key: :quota_order_number_id,
+                                primary_key: :quota_order_number_id do |ds|
+    ds.with_actual(QuotaDefinition)
+  end
 
   one_to_one :quota_order_number_origin, primary_key: :quota_order_number_sid,
                                          key: :quota_order_number_sid
