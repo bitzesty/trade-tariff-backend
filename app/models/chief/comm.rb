ae4cfd964989504bd2a8c8c68aa85091e8fbf0d8
Saulius Grigaliunas <saulius@ninja.lt>
2013-01-23 11:43:16 +0200

    Fix datasets. And add unit of quantity associations.

--- app/models/chief/comm.rb
+++ app/models/chief/comm.rb
@@ -1,6 +1,18 @@
 module Chief
   class Comm < Sequel::Model
     set_dataset db[:chief_comm].
-                order(:audit_tsmp.asc)
+                order(:fe_tsmp.asc)
+
+    set_primary_key [:fe_tsmp, :cmdty_code, :audit_tsmp]
+
+    one_to_one :first_unit_of_quantity, class_name: 'Chief::Unoq',
+                                        key: :tbl_code,
+                                        primary_key: :uoq_code_cdu1
+    one_to_one :second_unit_of_quantity, class_name: 'Chief::Unoq',
+                                         key: :tbl_code,
+                                         primary_key: :uoq_code_cdu2
+    one_to_one :third_unit_of_quantity, class_name: 'Chief::Unoq',
+                                        key: :tbl_code,
+                                        primary_key: :uoq_code_cdu3
   end
 end
