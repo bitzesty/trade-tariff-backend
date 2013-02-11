97bc77010bcf33ba3abf743623a6f70196832ea9
Saulius Grigaliunas <saulius@ninja.lt>
2013-02-11 10:34:49 +0200

    Expose first national quantity with national measurement unit set.

--- spec/models/national_measurement_unit_set_spec.rb
+++ spec/models/national_measurement_unit_set_spec.rb
@@ -4,8 +4,10 @@ describe NationalMeasurementUnitSet do
   describe "#national_measurement_unit_set_units" do
     let(:tbl1) { create :tbl9, :unoq }
     let(:tbl2) { create :tbl9, :unoq }
-    let(:comm) { create :comm, uoq_code_cdu2: tbl1.tbl_code,
-                               uoq_code_cdu3: tbl2.tbl_code }
+    let(:tbl3) { create :tbl9, :unoq }
+    let(:comm) { create :comm, uoq_code_cdu1: tbl1.tbl_code,
+                               uoq_code_cdu2: tbl2.tbl_code,
+                               uoq_code_cdu3: tbl3.tbl_code }
 
     let(:nmus) { NationalMeasurementUnitSet.where(cmdty_code: comm.cmdty_code).first }
 
@@ -17,17 +19,24 @@ describe NationalMeasurementUnitSet do
       nmus.national_measurement_unit_set_units.all?{ |nmusu| nmusu.is_a?(NationalMeasurementUnit) }
     end
 
-    it 'should set second national measurement unit code/description to comm uoq_code_cdu2' do
+    it 'should set first national measurement unit code/description to comm uoq_code_cdu1' do
       nmusu = nmus.national_measurement_unit_set_units.first
       nmusu.measurement_unit_code.should eq tbl1.tbl_code
       nmusu.description.should eq tbl1.tbl_txt
+      nmusu.level.should eq 1
+    end
+
+    it 'should set second national measurement unit code/description to comm uoq_code_cdu2' do
+      nmusu = nmus.national_measurement_unit_set_units.second
+      nmusu.measurement_unit_code.should eq tbl2.tbl_code
+      nmusu.description.should eq tbl2.tbl_txt
       nmusu.level.should eq 2
     end
 
     it 'should set second national measurement unit code/description to comm uoq_code_cdu3' do
       nmusu = nmus.national_measurement_unit_set_units.last
-      nmusu.measurement_unit_code.should eq tbl2.tbl_code
-      nmusu.description.should eq tbl2.tbl_txt
+      nmusu.measurement_unit_code.should eq tbl3.tbl_code
+      nmusu.description.should eq tbl3.tbl_txt
       nmusu.level.should eq 3
     end
   end
