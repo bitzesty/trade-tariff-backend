787a6b33bbaf70cb58ba23013eabc0922d31494f
Saulius Grigaliunas <saulius@ninja.lt>
2013-03-18 10:14:28 +0200

    Missing migration, bad merge

---
+++ 20130212144512_fix_chief_hectolitre_mappings.rb
@@ -0,0 +1,9 @@
+Sequel.migration do
+  up do
+    run "UPDATE measure_components SET measurement_unit_code='HLT', measurement_unit_qualifier_code=NULL WHERE measurement_unit_code='ASX' AND measurement_unit_qualifier_code = 'X'"
+  end
+
+  down do
+    # this is not reversible and should not be
+  end
+end
