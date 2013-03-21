44248d705b89f7a645bd781f8e7dac0770b46512
Saulius Grigaliunas <saulius@ninja.lt>
2013-03-21 12:02:50 +0200

    Remove db/schema.rb
    Sequel does not yet support view creation in schema file.

--- jenkins.sh
+++ jenkins.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -x
 bundle install --path "/home/jenkins/bundles/${JOB_NAME}" --deployment
-RAILS_ENV=test bundle exec rake db:test:prepare
+RAILS_ENV=test bundle exec rake db:create
+RAILS_ENV=test bundle exec rake db:migrate
 RAILS_ENV=test bundle exec rake ci:setup:rspec spec
 RESULT=$?
 exit $RESULT
