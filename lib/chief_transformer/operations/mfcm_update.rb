5e020008baf00341ca237de6e39271835eb6be84
Saulius Grigaliunas <saulius@ninja.lt>
2013-05-15 14:04:01 +0300

    Disable conformance validations for Mfmc update

--- lib/chief_transformer/operations/mfcm_update.rb
+++ lib/chief_transformer/operations/mfcm_update.rb
@@ -9,7 +9,8 @@ class ChiefTransformer
                  .with_gono_id(record.cmdty_code)
                  .valid_to(record.le_tsmp)
                  .each do |measure|
-                   measure.update validity_end_date: record.le_tsmp
+                   measure.validity_end_date = record.le_tsmp
+                   measure.save(validate: false)
                  end
         else
           if Measure.with_measure_type(record.measure_type)
