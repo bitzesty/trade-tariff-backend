2e517321a16ce07e7a213962806d007f891b6d31
James Abley <james.abley@gmail.com>
2013-06-25 10:20:20 +0100

    Only point to local thing now
    No longer using support.cluster in production

--- config/initializers/tire.rb
+++ config/initializers/tire.rb
@@ -1,7 +1,1 @@
-if Rails.env.production?
-  Tire.configure {url "http://support.cluster:9200"}
-elsif Rails.env.development?
-  Tire.configure {url "http://localhost:9200"}
-else
-  Tire.configure {url "http://localhost:9200"}
-end
+Tire.configure { url "http://localhost:9200" }
