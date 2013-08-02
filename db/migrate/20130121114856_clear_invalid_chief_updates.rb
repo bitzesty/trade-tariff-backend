fff536aa405ad7c652c37eb5b60c6a5fdc59dc24
Saulius Grigaliunas <saulius@ninja.lt>
2013-08-02 15:06:14 +0300

    Remove other unnecessary migrations

--- db/migrate/20130121114856_clear_invalid_chief_updates.rb
+++
@@ -1,8 +0,0 @@
-Sequel.migration do
-  up do
-    run "DELETE FROM `tariff_updates` WHERE ((`tariff_updates`.`update_type` IN ('TariffSynchronizer::ChiefUpdate')) AND (`issue_date` >= '2013-01-01') AND (`state` = 'M'))"
-  end
-
-  down do
-  end
-end
