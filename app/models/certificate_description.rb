2857df79ebad686d8d34790d14632a7d128f44f4
Saulius Grigaliunas <saulius@ninja.lt>
2013-09-10 11:10:14 +0300

    Fix national Certificate description
    Happened due to mass rename in static_national_data_insert.sql when
    moving to point in time architecture

--- app/models/certificate_description.rb
+++ app/models/certificate_description.rb
@@ -4,6 +4,10 @@ class CertificateDescription < Sequel::Model
   plugin :conformance_validator
 
   set_primary_key [:certificate_description_period_sid]
+
+  def to_s
+    description
+  end
 end
 
 
