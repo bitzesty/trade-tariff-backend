1d52484a1fac53abcf97903b91e0b582f3e5462c
Matthew Ford <matt@bitzesty.com>
2013-09-11 13:05:10 +0100

    Need to specify an env for GDS SSO

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,5 +1,8 @@
 set :output, { error: 'log/cron.error.log', standard: 'log/cron.log'}
 
+# We need Rake to use our own environment
+job_type :rake, "cd :path && govuk_setenv tariff-api bundle exec rake :task --silent :output"
+
 every 1.hour do
   rake "tariff:sync:apply"
 end
