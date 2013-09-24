2617e6738a88555c8688e98a7f19be3486e6cbf3
Saulius Grigaliunas <saulius@ninja.lt>
2013-09-24 10:39:36 +0300

    Expose all Tariff updates for the admin app
    Not just the latest ones for the frontend

--- spec/controllers/api/v1/updates_controller_spec.rb
+++ spec/controllers/api/v1/updates_controller_spec.rb
@@ -4,14 +4,21 @@ describe Api::V1::UpdatesController, "GET #index" do
   render_views
 
   let(:pattern) {
-    [{ update_type: String,
-      state: String,
-      filename: String}.ignore_extra_keys!].ignore_extra_values!
+    [
+     { update_type: "TariffSynchronizer::TaricUpdate",
+       state: String,
+       filename: String
+     }.ignore_extra_keys!,
+     { update_type: "TariffSynchronizer::TaricUpdate",
+       state: String,
+       filename: String
+     }.ignore_extra_keys!
+    ].ignore_extra_values!
   }
 
   context 'when records are present' do
-    let!(:chief_update) { create :chief_update, :applied }
-    let!(:taric_update) { create :taric_update, :applied }
+    let!(:taric_update1) { create :taric_update, :applied, issue_date: Date.yesterday }
+    let!(:taric_update2) { create :taric_update, :pending, issue_date: Date.today }
 
     it 'returns rendered records' do
       get :index, format: :json
@@ -23,6 +30,35 @@ describe Api::V1::UpdatesController, "GET #index" do
   context 'when records are not present' do
     it 'returns blank array' do
       get :index, format: :json
+
+      JSON.parse(response.body).should eq []
+    end
+  end
+end
+
+describe Api::V1::UpdatesController, "GET #latest" do
+  render_views
+
+  let(:pattern) {
+    [{ update_type: String,
+      state: String,
+      filename: String}.ignore_extra_keys!].ignore_extra_values!
+  }
+
+  context 'when records are present' do
+    let!(:chief_update) { create :chief_update, :applied }
+    let!(:taric_update) { create :taric_update, :applied }
+
+    it 'returns rendered records' do
+      get :latest, format: :json
+
+      response.body.should match_json_expression pattern
+    end
+  end
+
+  context 'when records are not present' do
+    it 'returns blank array' do
+      get :latest, format: :json
 
       JSON.parse(response.body).should eq []
     end
