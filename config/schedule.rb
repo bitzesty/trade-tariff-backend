b21cac649e2907a53784fb0f15fdf8ae08441552
Matthew Ford <matt@bitzesty.com>
2014-03-18 17:02:48 +0000

    Only run tariff sync once a day at 01:23
    In the interest of other application using mysql and elasticsearch
    to reduce the load, only run the sync during the night.
    
    If something urgent is released and needs indexing we can run it manually

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,6 +3,6 @@ set :output, { error: 'log/cron.error.log', standard: 'log/cron.log'}
 # We need Rake to use our own environment
 job_type :rake, "cd :path && /usr/local/bin/govuk_setenv tariff-api bundle exec rake :task --silent :output"
 
-every 1.hour do
+every :day, :at => '01:23am' do
   rake "tariff:sync:apply"
 end
