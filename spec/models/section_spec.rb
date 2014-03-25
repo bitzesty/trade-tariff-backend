a26869c4442ef6e9b2c7f82818666ccf8a718608
Matthew Ford <matt@bitzesty.com>
2014-03-25 14:24:38 +0000

    Fix pending spec for section.chapters with hidden codes

--- spec/models/section_spec.rb
+++ spec/models/section_spec.rb
@@ -3,8 +3,13 @@ require 'spec_helper'
 describe Section do
   describe 'associations' do
     describe 'chapters' do
+      let!(:chapter) { create(:chapter, :with_section) }
+
       it 'does not include HiddenGoodsNomenclatures' do
-        pending
+        section = chapter.section
+        create(:hidden_goods_nomenclature, goods_nomenclature_item_id: chapter.goods_nomenclature_item_id)
+
+        expect(section.chapters).to eq []
       end
     end
   end
