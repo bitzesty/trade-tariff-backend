a7e7007f8b47a85c68a00aef5d7a3f0f90cc12ba
Mario Andrés Correa <a@macool.me>
2014-04-16 12:35:03 -0500

    Do not show section duplicates when searching via synonyms
    [pivotal story](https://www.pivotaltracker.com/n/projects/488191/stories/68999382)

--- app/services/search_service/fuzzy_search.rb
+++ app/services/search_service/fuzzy_search.rb
@@ -9,6 +9,15 @@ class SearchService
       begin
         @results ||= begin
           FuzzySearchResult.new(query_string, date).each_with_object({}) do |(match_type, search_index, results), memo|
+
+            results.uniq! do |result|
+              if result["_source"].has_key?("reference")
+                result["_source"]["reference"]["id"]
+              else
+                result
+              end
+            end
+
             memo.deep_merge!({
               match_type => { search_index.type.pluralize => results }
             })
