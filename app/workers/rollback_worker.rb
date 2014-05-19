1146db3fdf267e258d7b1d23301298cd4ddf7844
Matthew Ford <matt@bitzesty.com>
2014-05-19 18:55:57 +0100

    Change the sidekiq retry job to match the sync schedule.
    The sync is run each hour, so we should retry on the same schedule.
    If it fails for a day then we should stop trying.

--- app/workers/rollback_worker.rb
+++ app/workers/rollback_worker.rb
@@ -3,6 +3,14 @@ class RollbackWorker
 
   sidekiq_options queue: :rollbacks
 
+  # Retry the job for 24 hours, after that the rollback has to be be recreated.
+  sidekiq_options retry: 24
+
+  # Retry the job every hour (the sync runs each hour)
+  sidekiq_retry_in do |count|
+    3600 
+  end
+
   def perform(date, redownload = false)
     TariffSynchronizer.rollback(date, redownload)
   end
