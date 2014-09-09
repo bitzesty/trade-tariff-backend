6dce594f4e9ba4a72957c60418185e8193b19699
Matthew Ford <matt@bitzesty.com>
2014-09-09 17:29:26 +0100

    use docker redis port/url if present

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,10 +2,16 @@
 
 debug_redis = false
 
+url = if ENV['TRADETARIFFBACKEND_REDIS_1_PORT_6379_TCP'].present?
+    ENV['TRADETARIFFBACKEND_REDIS_1_PORT_6379_TCP']
+  else
+    "redis://localhost:6379"
+  end
+
 Sidekiq.configure_client do |config|
-  config.redis = { :namespace => 'tariff-api', logger: Logger.new(debug_redis ? $stdout : "/dev/null") }
+  config.redis = { url: url, namespace: 'tariff-api', logger: Logger.new(debug_redis ? $stdout : "/dev/null") }
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { :namespace => 'tariff-api' }
+  config.redis = { url: url, namespace: 'tariff-api' }
 end
