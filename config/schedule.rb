aa709b97c404989c84c12ce3905b91bfd12e5271
Mario Andrés Correa <a@macool.me>
2014-10-07 17:51:40 -0500

    we don't need `govuk_setenv` on this env

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :output, { error: 'log/cron.error.log', standard: 'log/cron.log'}
 
 # We need Rake to use our own environment
-job_type :rake, "cd :path && /usr/local/bin/govuk_setenv tariff-api bundle exec rake :task --silent :output"
+job_type :rake, "cd :path && bundle exec rake :task --silent :output"
 
 every 1.hour do
   rake "tariff:sync:apply"
