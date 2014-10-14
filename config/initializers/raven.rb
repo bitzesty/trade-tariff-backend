84ea7c9aa015a2214748924fb062121e465a4c5b
Mario Andrés Correa <a@macool.me>
2014-10-13 21:52:19 -0500

    log development errors

--- config/initializers/raven.rb
+++ config/initializers/raven.rb
@@ -1,5 +1,6 @@
 require "raven"
 
 Raven.configure do |config|
+  config.environments = %w[ development ]
   config.dsn = "https://3bee46b6981347afbf075c2179190c94:1cdaa52a99be454eb995830c1306b811@app.getsentry.com/30337"
 end
