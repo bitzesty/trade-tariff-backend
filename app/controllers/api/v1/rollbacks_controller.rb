5e8c00146c903aa5fbab73e3fcedc880511a1487
Mario Andrés Correa <a@macool.me>
2015-02-25 22:00:04 -0500

    paginate rollbacks
    [podio story](https://podio.com/bitzesty/one-space/apps/stories/items/15)

--- app/controllers/api/v1/rollbacks_controller.rb
+++ app/controllers/api/v1/rollbacks_controller.rb
@@ -2,9 +2,12 @@ module Api
   module V1
     class RollbacksController < ApiController
       before_filter :authenticate_user!
+      after_filter :paginate, only: :index
+
+      PER_PAGE = 20
 
       def index
-        @rollback_jobs = Rollback.descending
+        @rollback_jobs = collection
       end
 
       def create
@@ -22,6 +25,22 @@ module Api
 
       def rollback_params
         params.require(:rollback).permit(:date, :keep, :reason, :user_id)
+      end
+
+      def collection
+        @collection ||= Rollback.descending.paginate(page, PER_PAGE)
+      end
+
+      def paginate
+        response.headers["X-Pagination"] = {
+          page: page,
+          per_page: PER_PAGE,
+          total_count: collection.pagination_record_count
+        }.to_json
+      end
+
+      def page
+        Integer(params[:page] || 1)
       end
     end
   end
