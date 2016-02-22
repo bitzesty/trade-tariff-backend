ab5b800a8e985134bfe734cecee2694501c08ce0
Hildebrando Rueda <harq.soft@gmail.com>
2016-02-22 00:27:25 -0500

    Fix missing if function from postgres, replace with case

--- app/models/measure.rb
+++ app/models/measure.rb
@@ -144,15 +144,15 @@ class Measure < Sequel::Model
   dataset_module do
     def with_base_regulations
       select(Sequel.expr(:measures).*).
-      select_append(Sequel.as(Sequel.function(:if, Sequel.lit('measures.validity_start_date IS NOT NULL'), Sequel.lit('measures.validity_start_date'), Sequel.lit('base_regulations.validity_start_date')), :effective_start_date)).
-      select_append(Sequel.as(Sequel.function(:if, Sequel.lit('measures.validity_end_date IS NOT NULL'), Sequel.lit('measures.validity_end_date'), Sequel.lit('base_regulations.effective_end_date')), :effective_end_date)).
+      select_append(Sequel.as(Sequel.case({{Sequel.qualify(:measures, :validity_start_date)=>nil}=>Sequel.lit('base_regulations.validity_start_date')}, Sequel.lit('measures.validity_start_date')), :effective_start_date)).
+      select_append(Sequel.as(Sequel.case({{Sequel.qualify(:measures, :validity_end_date)=>nil}=>Sequel.lit('base_regulations.effective_end_date')}, Sequel.lit('measures.validity_end_date')), :effective_end_date)).
       join_table(:inner, :base_regulations, base_regulations__base_regulation_id: :measures__measure_generating_regulation_id)
     end
 
     def with_modification_regulations
       select(Sequel.expr(:measures).*).
-      select_append(Sequel.as(Sequel.function(:if, Sequel.lit('measures.validity_start_date IS NOT NULL'), Sequel.lit('measures.validity_start_date'), Sequel.lit('modification_regulations.validity_start_date')), :effective_start_date)).
-      select_append(Sequel.as(Sequel.function(:if, Sequel.lit('measures.validity_end_date IS NOT NULL'), Sequel.lit('measures.validity_end_date'), Sequel.lit('modification_regulations.effective_end_date')), :effective_end_date)).
+      select_append(Sequel.as(Sequel.case({{Sequel.qualify(:measures, :validity_start_date)=>nil}=>Sequel.lit('modification_regulations.validity_start_date')}, Sequel.lit('measures.validity_start_date')), :effective_start_date)).
+      select_append(Sequel.as(Sequel.case({{Sequel.qualify(:measures, :validity_end_date)=>nil}=>Sequel.lit('modification_regulations.validity_start_date')}, Sequel.lit('measures.validity_end_date')), :effective_end_date)).
       join_table(:inner, :modification_regulations, modification_regulations__modification_regulation_id: :measures__measure_generating_regulation_id)
     end
 
