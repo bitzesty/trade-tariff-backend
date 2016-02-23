4f1437b3f445750b0d0f7cf4a776b87900142762
Hildebrando Rueda <harq.soft@gmail.com>
2016-02-23 13:34:59 -0500

    Route constraint fix

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     # How (or even if) API versioning will be implemented is still an open question. We can defer
     # the choice until we need to expose the API to clients which we don't control.
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :sections, only: [:index, :show], constraints: { id: /\d{1,2}/ } do
+      resources :sections, only: [:index, :show], constraints: { id: /\d{1,3}/ } do
         scope module: 'sections', constraints: { section_id: /\d{1,2}/, id: /\d+/ } do
           resource :section_note, only: [:show, :create, :update, :destroy]
           resources :search_references, only: [:show, :index, :destroy, :create, :update]
