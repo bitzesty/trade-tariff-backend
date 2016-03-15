645f00ccd15df9fd4a7abe7c318a3c7df3d07c22
Hildebrando Rueda <harq.soft@gmail.com>
2016-03-14 21:02:07 -0500

    Update tests and the update operation
    sequel method calls ‘update’ method which calls ‘save_changes’ and this
    method:
    “If the object has not been modified, returns nil”
    so it happens that there is a repeated record, and we want to check
    this model too

--- spec/integration/taric_importer/record_processor/update_operation_spec.rb
+++ spec/integration/taric_importer/record_processor/update_operation_spec.rb
@@ -15,37 +15,32 @@ describe TaricImporter::RecordProcessor::UpdateOperation do
 
   describe '#call' do
     let(:operation_date) { Date.new(2013,8,1) }
-    let(:record) {
-      TaricImporter::RecordProcessor::Record.new(record_hash)
-    }
-
-    let(:operation) {
-      TaricImporter::RecordProcessor::UpdateOperation.new(record, operation_date)
-    }
+    let(:record) { TaricImporter::RecordProcessor::Record.new(record_hash) }
 
     context 'record for update present' do
-      before {
-        create :language_description, language_code_id: 'FR',
-                                      language_id: 'EN',
-                                      description: 'French'
-
-        LanguageDescription.unrestrict_primary_key
-      }
+      before { LanguageDescription.unrestrict_primary_key }
 
       it 'identifies as create operation' do
-        operation.call
-
+        create_language_description_record
+        build_update_operation.call
         expect(LanguageDescription.count).to eq 1
         expect(LanguageDescription.first.description).to eq 'French!'
       end
 
       it 'returns model instance' do
-        expect(operation.call).to be_kind_of LanguageDescription
+        create_language_description_record
+        expect(build_update_operation.call).to be_kind_of LanguageDescription
+      end
+
+      it 'returns model instance even when the previous record is equal' do
+        create_language_description_record
+        build_update_operation.call
+        expect(build_update_operation.call).to be_kind_of LanguageDescription
       end
 
       it 'sets update operation date to operation_date' do
-        operation.call
-
+        create_language_description_record
+        build_update_operation.call
         expect(
           LanguageDescription::Operation.where(operation: 'U').first.operation_date
         ).to eq operation_date
@@ -54,8 +49,17 @@ describe TaricImporter::RecordProcessor::UpdateOperation do
 
     context 'record for update missing' do
       it 'raises Sequel::RecordNotFound exception' do
-        expect { operation.call }.to raise_error(Sequel::RecordNotFound)
+        expect { build_update_operation.call }.to raise_error(Sequel::RecordNotFound)
       end
+    end
+
+
+    def build_update_operation
+      TaricImporter::RecordProcessor::UpdateOperation.new(record, operation_date)
+    end
+
+    def create_language_description_record
+      create :language_description, language_code_id: 'FR', language_id: 'EN', description: 'French'
     end
   end
 end
