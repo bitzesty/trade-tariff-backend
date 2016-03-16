20b49ceaaf439c2996784b0ce8efd8065ee15fd7
Hildebrando Rueda <harq.soft@gmail.com>
2016-03-16 01:01:46 -0500

    Update variable names

--- spec/unit/tariff_synchronizer/taric_file_name_generator_spec.rb
+++ spec/unit/tariff_synchronizer/taric_file_name_generator_spec.rb
@@ -3,11 +3,11 @@ require 'tariff_synchronizer/taric_file_name_generator'
 
 describe TaricFileNameGenerator do
   let(:example_date){ Date.new(2010,1,1) }
-  let(:chief_file_name) { TaricFileNameGenerator.new(example_date) }
+  let(:taric_file_name_generator) { TaricFileNameGenerator.new(example_date) }
 
   describe "#url" do
     it "returns the expected url to have the taric file for a specific date" do
-      expect(chief_file_name.url).to eq("/taric/TARIC320100101")
+      expect(taric_file_name_generator.url).to eq("/taric/TARIC320100101")
     end
   end
 end
