ecac0a1da29192768a1238fde3371b430973ee99
Hildebrando Rueda <harq.soft@gmail.com>
2016-03-17 22:47:30 -0500

    Remove testing environment check

--- spec/support/tariff_synchronizer_defaults.rb
+++ spec/support/tariff_synchronizer_defaults.rb
@@ -2,3 +2,4 @@ TariffSynchronizer.request_throttle = 1
 TariffSynchronizer.retry_count = 5
 TariffSynchronizer.exception_retry_count = 5
 TariffSynchronizer.host = "http://example.com"
+TariffSynchronizer.root_path = "tmp/data"
