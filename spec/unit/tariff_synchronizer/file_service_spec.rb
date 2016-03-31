e9405a4e1e333d2c1c8d83088a35177e737bfc26
Hildebrando Rueda <harq.soft@gmail.com>
2016-03-31 18:27:05 -0500

    allow_any_instance_of is discourage, remove these lines
    https://relishapp.com/rspec/rspec-mocks/docs/working-with-legacy-code/an
    y-instance

--- spec/unit/tariff_synchronizer/file_service_spec.rb
+++ spec/unit/tariff_synchronizer/file_service_spec.rb
@@ -11,21 +11,21 @@ describe TariffSynchronizer::FileService do
   describe ".download_content" do
     context "partial content received" do
       it "raises DownloadException" do
-        allow_any_instance_of(Curl::Easy).to receive(:perform).and_raise(Curl::Err::PartialFileError)
+        stub_request(:get, "http://example/test").to_raise(Curl::Err::PartialFileError)
         expect { klass.download_content("http://example/test") }.to raise_error TariffSynchronizer::FileService::DownloadException
       end
     end
 
     context "unable to connect" do
       it "raises DownloadException" do
-        allow_any_instance_of(Curl::Easy).to receive(:perform).and_raise(Curl::Err::ConnectionFailedError)
+        stub_request(:get, "http://example/test").to_raise(Curl::Err::ConnectionFailedError)
         expect { klass.download_content("http://example/test") }.to raise_error TariffSynchronizer::FileService::DownloadException
       end
     end
 
     context "host resolution error" do
       it "raises DownloadException" do
-        allow_any_instance_of(Curl::Easy).to receive(:perform).and_raise(Curl::Err::HostResolutionError)
+        stub_request(:get, "http://example/test").to_raise(Curl::Err::HostResolutionError)
         expect { klass.download_content("http://example/test") }.to raise_error TariffSynchronizer::FileService::DownloadException
       end
     end
