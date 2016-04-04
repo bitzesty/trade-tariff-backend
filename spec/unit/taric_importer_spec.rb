8daedd113492b4571cab2b4f5bfdc5742c66cb01
Hildebrando Rueda <harq.soft@gmail.com>
2016-04-04 15:55:21 -0500

    Move spec, this is not a integration spec so move it
    This is not a integration spec, the only entity tested in the import
    method of the taric importer class, move this spec to its unit test.

--- spec/unit/taric_importer_spec.rb
+++ spec/unit/taric_importer_spec.rb
@@ -3,23 +3,69 @@ require "taric_importer"
 
 describe TaricImporter do
   describe "#import" do
-    context "when provided with valid taric file" do
-      it 'instantiates appropriate processing strategy' do
-        allow_any_instance_of(
-          TaricImporter::RecordProcessor
-        ).to receive(:process!)
-        path = "spec/fixtures/taric_samples/footnote.xml"
-        importer = TaricImporter.new(path)
-        importer.import(validate: false)
+    context "on parsing error" do
+      it 'raises TaricImportException' do
+        invalid_file = "spec/fixtures/taric_samples/broken_insert_record.xml"
+        importer = TaricImporter.new(invalid_file)
+        expect { importer.import }.to raise_error TaricImporter::ImportException
       end
     end
 
-    context "on parsing error" do
-      let(:broken_insert_record) { "spec/fixtures/taric_samples/broken_insert_record.xml" }
+    context "on a complex record like measure" do
+      let(:example_date) { Date.new(2013,8,2) }
+      let(:taric_file_path) { "spec/fixtures/taric_samples/create_measure.xml" }
+      let(:taric_update_path) { "spec/fixtures/taric_samples/update_measure.xml" }
 
-      it 'raises TaricImportException' do
-        importer = TaricImporter.new(broken_insert_record)
-        expect { importer.import }.to raise_error TaricImporter::ImportException
+      before { Measure.unrestrict_primary_key }
+      after { Measure.restrict_primary_key }
+
+      it "imports single Measure" do
+        TaricImporter.new(taric_file_path, example_date).import
+        expect(Measure.count).to eq 1
+      end
+
+      it "imports single Measure and updates it" do
+        TaricImporter.new(taric_file_path, example_date).import
+        TaricImporter.new(taric_update_path, example_date).import
+        expect(Measure.count).to eq 1
+      end
+
+      it "creates single Measure::Operation(oplog) entry" do
+        TaricImporter.new(taric_file_path, example_date).import
+
+        expect(Measure::Operation.count).to eq 1
+        expect(
+          Measure::Operation.where(
+            operation: 'C',
+            measure_sid: '3318239',
+            operation_date: example_date
+          ).first
+        ).to be_present
+      end
+
+      it "creates two Measure::Operation(oplog) entries after update" do
+        TaricImporter.new(taric_file_path, example_date).import
+        TaricImporter.new(taric_update_path, example_date).import
+
+        expect(Measure::Operation.count).to eq 2
+        update = Measure::Operation.where(
+            operation: 'U',
+            measure_sid: '3318239',
+            operation_date: example_date,
+          ).first
+        expect(update).to be_present
+        expect(update.validity_end_date).to be_present
+      end
+
+      it "emits conformance errors as ActiveSupport notifications" do
+        bogus_records = []
+        ActiveSupport::Notifications.subscribe /conformance_error/ do |*args|
+          event = ActiveSupport::Notifications::Event.new(*args)
+          bogus_records << event.payload[:record]
+        end
+
+        TaricImporter.new(taric_file_path, example_date).import
+        expect(bogus_records.size).to eq 1
       end
     end
   end
