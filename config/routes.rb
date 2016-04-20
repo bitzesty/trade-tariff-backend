85b2248a433f41cd05e17a382360a713e1349a3a
Hildebrando Rueda <harq.soft@gmail.com>
2016-04-20 15:56:08 -0500

    Restore constraints of id length

--- config/routes.rb
+++ config/routes.rb
@@ -7,40 +7,40 @@ Rails.application.routes.draw do
     # How (or even if) API versioning will be implemented is still an open question. We can defer
     # the choice until we need to expose the API to clients which we don't control.
     scope module: :v1, constraints: ApiConstraints.new(version: 1, default: true) do
-      resources :sections, only: [:index, :show], constraints: { id: /\d+/ } do
-        scope module: 'sections', constraints: { section_id: /\d+/, id: /\d+/ } do
+      resources :sections, only: [:index, :show], constraints: { id: /\d{1,2}/ } do
+        scope module: 'sections', constraints: { section_id: /\d{1,2}/, id: /\d+/ } do
           resource :section_note, only: [:show, :create, :update, :destroy]
           resources :search_references, only: [:show, :index, :destroy, :create, :update]
         end
       end
 
-      resources :chapters, only: [:index, :show], constraints: { id: /\d+/ } do
+      resources :chapters, only: [:index, :show], constraints: { id: /\d{2}/ } do
         member {
           get :changes
         }
 
-        scope module: 'chapters', constraints: { chapter_id: /\d+/, id: /\d+/ } do
+        scope module: 'chapters', constraints: { chapter_id: /\d{2}/, id: /\d+/ } do
           resource :chapter_note, only: [:show, :create, :update, :destroy]
           resources :search_references, only: [:show, :index, :destroy, :create, :update]
         end
       end
 
-      resources :headings, only: [:show], constraints: { id: /\d+/ } do
+      resources :headings, only: [:show], constraints: { id: /\d{4}/ } do
         member {
           get :changes
         }
 
-        scope module: 'headings', constraints: { heading_id: /\d+/, id: /\d+/ } do
+        scope module: 'headings', constraints: { heading_id: /\d{4}/, id: /\d+/ } do
           resources :search_references, only: [:show, :index, :destroy, :create, :update]
         end
       end
 
-      resources :commodities, only: [:show], constraints: { id: /\d+/ } do
+      resources :commodities, only: [:show], constraints: { id: /\d{10}/ } do
         member {
           get :changes
         }
 
-        scope module: 'commodities', constraints: { commodity_id: /\d+/, id: /\d+/ } do
+        scope module: 'commodities', constraints: { commodity_id: /\d{10}/, id: /\d+/ } do
           resources :search_references, only: [:show, :index, :destroy, :create, :update]
         end
       end
