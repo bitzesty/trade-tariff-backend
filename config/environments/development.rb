31c831739a5f531abd8e2c3c7f5ddb90ef9c03da
Hildebrando Rueda <harq.soft@gmail.com>
2016-04-21 12:46:37 -0500

    Trap emails in development with mailcatcher
    add key to env variables, default value for TARIFF_SYNC_EMAIL is set to
    trade-tariff-alerts@digital.cabinet-office.gov.uk, do not send emails
    to this address.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,6 +21,10 @@ Rails.application.configure do
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = false
 
+  # Mailcatcher configuration.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = { address: "localhost", port: 1025 }
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
