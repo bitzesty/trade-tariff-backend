5278634387639d94977eeeff10128b878b79517e
Hildebrando Rueda <harq.soft@gmail.com>
2016-06-22 15:48:56 -0500

    Replace memcached with memory store for caching

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, nil, { namespace: 'tariff', expires_in: 1.day, compress: true }
+  config.cache_store = :memory_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
