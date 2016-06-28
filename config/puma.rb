15070c5027638708633612db9926fefb8815e0bd
Matthew Ford <matt@bitzesty.com>
2016-06-28 14:01:40 +0100

    ensure puma establishes the connection on boot for SequelRails

--- config/puma.rb
+++ config/puma.rb
@@ -7,3 +7,15 @@ preload_app!
 rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
+
+on_worker_boot do
+  # Ensure we don't keep connections
+  if defined?(Sequel)
+    ::Sequel::Model.db.disconnect
+    ::Sequel::DATABASES.each{|db| db.disconnect }
+  end
+end
+
+after_worker_boot do
+  SequelRails.setup Rails.env if defined?(SequelRails)
+end
