b953593227cc92fdb07cd04d781edad0496e95b3
Hildebrando Rueda <harq.soft@gmail.com>
2016-07-01 00:42:10 -0500

    Avoid bad queries because invalid table names
    This change will allow to load the right table name when the class is
    loaded.
    
    https://github.com/jeremyevans/sequel/blob/3b82e3e539995bd40ba769638b00d
    f369ce0ac94/lib/sequel/model/base.rb#L163-L166

--- app/models/chief/measure_type_cond.rb
+++ app/models/chief/measure_type_cond.rb
@@ -1,6 +1,5 @@
 module Chief
-  class MeasureTypeCond < Sequel::Model
-    set_dataset db[:chief_measure_type_cond]
+  class MeasureTypeCond < Sequel::Model(:chief_measure_type_cond)
     set_primary_key [:measure_group_code, :measure_type]
   end
 end
