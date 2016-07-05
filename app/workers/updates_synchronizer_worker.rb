7a831b350ba334af7b76897815470d3f29ad8a18
Hildebrando Rueda <harq.soft@gmail.com>
2016-07-05 17:45:00 -0500

    Update sidekiq workers (#48)
    * Do not store the backtrace in redis
    
    * Do not retry the UpdatesSynchronizerWorker

--- app/workers/updates_synchronizer_worker.rb
+++ app/workers/updates_synchronizer_worker.rb
@@ -1,5 +1,8 @@
 class UpdatesSynchronizerWorker
   include Sidekiq::Worker
+
+  sidekiq_options queue: :sync, retry: false
+
   def perform
     TariffSynchronizer.download
     TariffSynchronizer.apply
