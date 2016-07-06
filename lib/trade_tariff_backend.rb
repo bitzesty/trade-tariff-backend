a036317ee94cffd096d2bd017491228f029e1772
Hildebrando Rueda <harq.soft@gmail.com>
2016-07-05 19:12:17 -0500

    Update ses gem (#47)
    * Update gem to allow send emails through Amazon ses
    
    * Avoid sending emails to email, raise exception if env var not set
    
    * Set the sender email as an environment variable

--- lib/trade_tariff_backend.rb
+++ lib/trade_tariff_backend.rb
@@ -26,8 +26,13 @@ module TradeTariffBackend
     end
 
     # Email of the user who receives all info/error notifications
+    def from_email
+      ENV.fetch("TARIFF_FROM_EMAIL")
+    end
+
+    # Email of the user who receives all info/error notifications
     def admin_email
-      ENV.fetch("TARIFF_SYNC_EMAIL", "trade-tariff-alerts@digital.cabinet-office.gov.uk")
+      ENV.fetch("TARIFF_SYNC_EMAIL")
     end
 
     def platform
