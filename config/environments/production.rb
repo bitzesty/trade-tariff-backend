7572d1967004863762118f93d0254af8ab41ec82
Matthew Ford <matt@bitzesty.com>
2016-07-25 16:33:58 +0100

    use auth for memecache

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,8 +54,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, nil, { :namespace => ENV["GOVUK_APP_DOMAIN"], :expires_in => 1.day, :compress => true }
-
+  config.cache_store = :dalli_store, nil, { namespace: ENV["GOVUK_APP_DOMAIN"], expires_in: 1.day, compress: true, username: ENV["MEMCACHE_USER"], password: ENV["MEMCACHE_PASSWORD"] }
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
