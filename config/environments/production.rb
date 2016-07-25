53f2e50cfa110e6bc9b7792afa6a44d5794d4306
Matthew Ford <matt@bitzesty.com>
2016-07-25 15:16:49 +0100

    increase memory store useage

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
   # Use a different cache store in production.
-  config.cache_store = :memory_store
+  config.cache_store = :memory_store, { size: 100.megabytes }
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
