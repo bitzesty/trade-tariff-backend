c9373b9c0e39523560e7d555160cd1b66f8d2396
Hildebrando Rueda <harq.soft@gmail.com>
2016-08-03 15:31:23 -0500

    Remove legacy configuration in the application.rb file

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,6 @@ module TradeTariffBackend
   class Application < Rails::Application
     require 'trade_tariff_backend'
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-
     # lib directory to be autoloadable.
     config.autoload_paths << "#{Rails.root}/lib"
 
@@ -24,26 +20,10 @@ module TradeTariffBackend
       g.test_framework false
     end
 
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'UTC'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
-    config.i18n.enforce_available_locales = true
-
-    # Configure the default encoding used in templates for Ruby 1.9.
-    config.encoding = "utf-8"
 
     # Enable the asset pipeline
     config.assets.enabled = false
-
-    # Version of your assets, change this if you want to expire all your assets
-    # config.assets.version = '1.0'
-
-    # Disable Rack::Cache.
-    config.action_dispatch.rack_cache = nil
 
     # Configure sequel
     config.sequel.schema_format = :sql
