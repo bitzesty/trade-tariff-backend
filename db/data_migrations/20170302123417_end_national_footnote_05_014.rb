3e15a5bf63764c661072a34c92e5fb471174076f
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-03-14 18:21:31 +0300

    fix data migration

--- db/data_migrations/20170302123417_end_national_footnote_05_014.rb
+++ db/data_migrations/20170302123417_end_national_footnote_05_014.rb
@@ -16,27 +16,27 @@ TradeTariffBackend::DataMigrator.migration do
     }
 
     apply {
-      Footnote.where(footnote_type_id: FOOTNOTE_TYPE_ID, footnote_id: FOOTNOTE_ID).where('validity_end_date <> ?', VALIDITY_END_DATE).all.each do |f|
+      Footnote.where(footnote_type_id: FOOTNOTE_TYPE_ID, footnote_id: FOOTNOTE_ID).where('validity_end_date <> ? OR validity_end_date IS NULL', VALIDITY_END_DATE).all.each do |f|
         f.update(validity_end_date: VALIDITY_END_DATE)
       end
 
-      FootnoteDescriptionPeriod.where(footnote_type_id: FOOTNOTE_TYPE_ID, footnote_id: FOOTNOTE_ID).where('validity_end_date <> ?', VALIDITY_END_DATE).all.each do |fdp|
+      FootnoteDescriptionPeriod.where(footnote_type_id: FOOTNOTE_TYPE_ID, footnote_id: FOOTNOTE_ID).where('validity_end_date <> ? OR validity_end_date IS NULL', VALIDITY_END_DATE).all.each do |fdp|
         fdp.update(validity_end_date: VALIDITY_END_DATE)
       end
 
-      FootnoteAssociationAdditionalCode.where(footnote_type_id: FOOTNOTE_TYPE_ID, footnote_id: FOOTNOTE_ID).where('validity_end_date <> ?', VALIDITY_END_DATE).all.each do |faac|
+      FootnoteAssociationAdditionalCode.where(footnote_type_id: FOOTNOTE_TYPE_ID, footnote_id: FOOTNOTE_ID).where('validity_end_date <> ? OR validity_end_date IS NULL', VALIDITY_END_DATE).all.each do |faac|
         faac.update(validity_end_date: VALIDITY_END_DATE)
       end
 
-      FootnoteAssociationGoodsNomenclature.where(footnote_type: FOOTNOTE_TYPE_ID, footnote_id: FOOTNOTE_ID).where('validity_end_date <> ?', VALIDITY_END_DATE).all.each do |fagn|
+      FootnoteAssociationGoodsNomenclature.where(footnote_type: FOOTNOTE_TYPE_ID, footnote_id: FOOTNOTE_ID).where('validity_end_date <> ? OR validity_end_date IS NULL', VALIDITY_END_DATE).all.each do |fagn|
         fagn.update(validity_end_date: VALIDITY_END_DATE)
       end
 
-      FootnoteAssociationMeursingHeading.where(footnote_type: FOOTNOTE_TYPE_ID, footnote_id: FOOTNOTE_ID).where('validity_end_date <> ?', VALIDITY_END_DATE).all.each do |famh|
+      FootnoteAssociationMeursingHeading.where(footnote_type: FOOTNOTE_TYPE_ID, footnote_id: FOOTNOTE_ID).where('validity_end_date <> ? OR validity_end_date IS NULL', VALIDITY_END_DATE).all.each do |famh|
         famh.update(validity_end_date: VALIDITY_END_DATE)
       end
 
-      FootnoteAssociationErn.where(footnote_type: FOOTNOTE_TYPE_ID, footnote_id: FOOTNOTE_ID).where('validity_end_date <> ?', VALIDITY_END_DATE).all.each do |fae|
+      FootnoteAssociationErn.where(footnote_type: FOOTNOTE_TYPE_ID, footnote_id: FOOTNOTE_ID).where('validity_end_date <> ? OR validity_end_date IS NULL', VALIDITY_END_DATE).all.each do |fae|
         fae.update(validity_end_date: VALIDITY_END_DATE)
       end
     }
