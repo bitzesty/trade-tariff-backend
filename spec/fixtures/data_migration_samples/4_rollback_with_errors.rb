1ff36267f0d3ff301e73fe5a03dcf9d4906149ec
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-04-04 15:56:10 +0300

    change specs for data migrator

---
+++ spec/fixtures/data_migration_samples/4_rollback_with_errors.rb
@@ -0,0 +1,19 @@
+TradeTariffBackend::DataMigrator.migration do
+  name "Rollback with errors"
+
+  up do
+    applicable   { Language.dataset.none? }
+    apply        {
+      Language.unrestrict_primary_key
+      Language.create(language_id: 'GB')
+    }
+  end
+
+  down do
+    applicable { Language.dataset.where(language_id: 'GB').any? }
+    apply {
+      Language.dataset.where(language_id: 'GB').destroy
+      Language.restrict_primary_key
+    }
+  end
+end
