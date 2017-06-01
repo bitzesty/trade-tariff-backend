8b5601d930f2c5fbcf86c38faacd6ea8cd459f4a
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-06-01 22:42:40 +0300

    add suggestions api, add suggestions service (commodity codes + search_reference titles), add specs

--- app/controllers/api/v1/search_controller.rb
+++ app/controllers/api/v1/search_controller.rb
@@ -4,6 +4,10 @@ module Api
       def search
         render json: SearchService.new(params)
       end
+
+      def suggestions
+        render json: SuggestionsService.new
+      end
     end
   end
 end
