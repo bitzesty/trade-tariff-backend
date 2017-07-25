6b7c8649f391e663ea42320a165293f3b8b072bc
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-07-25 22:53:43 +0300

    rename codes mapping file

--- app/services/search_service/codes_mapping.rb
+++ app/services/search_service/codes_mapping.rb
@@ -1,5 +1,8 @@
 require 'csv'
 
+# UK Trade Tariff: correlation tables
+# https://www.gov.uk/government/publications/uk-trade-tariff-correlation-tables/uk-trade-tariff-correlation-tables
+
 class SearchService
   module CodesMapping
     def self.check(query)
@@ -13,9 +16,9 @@ class SearchService
     private
 
     def self.data
-      Rails.cache.fetch('codes-mapping', expires_in: 24.hours) do
-        mappings = CSV.read(File.join(Rails.root, 'db', 'codes-mapping.csv'), col_sep: ';')
-        mappings.to_h
+      Rails.cache.fetch('codes-mapping-2016-to-2017', expires_in: 24.hours) do
+        file = File.join(Rails.root, 'db', 'codes-mapping-2016-to-2017.csv')
+        CSV.read(file, col_sep: ';').to_h
       end
     end
   end
