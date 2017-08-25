fac30a3e29013765f65c01f737b9f0ebe097dcff
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-08-25 12:03:00 +0300

    [rails 5.0.0] require sprockets to configure assets

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,7 @@ require File.expand_path('../boot', __FILE__)
 
 require "action_controller/railtie"
 require "action_mailer/railtie"
+require "sprockets/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
