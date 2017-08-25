0cf67d4574177776b4dc9ba49a344ca2efb7c703
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-08-25 16:16:32 +0300

    [rails 5.0.0] running task `rails app:update`

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,9 @@ Rails.application.configure do
 
   # Compress JavaScripts and CSS.
   config.assets.compress = true
+  config.assets.js_compressor = :uglifier
+  # config.assets.css_compressor = :sass
+
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
