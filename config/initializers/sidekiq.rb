6de27f06f9e6d0e18b91db045bcba9c9a2cc794f
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-09-20 17:23:55 +0300

    [redis] using cf redis-alpha service credentials for redis config

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,14 @@
 require "sidekiq"
 
-redis_config = Rails.application.config_for(:redis).symbolize_keys
+redis_url = begin
+  JSON.parse(ENV["VCAP_SERVICES"])["user-provided"].select{ |s| s["name"] == "redis-alpha" }[0]["credentials"]["uri"]
+rescue
+  ENV["REDIS_URL"]
+end
+
+redis_db = ENV["REDIS_DB"] || 0
+
+redis_config = { url: redis_url, db: redis_db }
 
 Sidekiq.configure_server do |config|
   config.redis = redis_config
