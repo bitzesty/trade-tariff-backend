d04de98e16219caa78bf680e64ec59aad77c920d
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-09-27 09:46:35 +0300

    [redis] redis will use old redis service and db == 0 for all envs

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,12 +1,17 @@
 require "sidekiq"
 
-redis_url = begin
-  JSON.parse(ENV["VCAP_SERVICES"])["user-provided"].select{ |s| s["name"] == "redis-alpha" }[0]["credentials"]["uri"]
-rescue
-  ENV["REDIS_URL"]
-end
+# PASS Redis is not ready for prod
+# redis_url = begin
+#   JSON.parse(ENV["VCAP_SERVICES"])["user-provided"].select{ |s| s["name"] == "redis-alpha" }[0]["credentials"]["uri"]
+# rescue
+#   ENV["REDIS_URL"]
+# end
 
-redis_db = ENV["REDIS_DB"] || 0
+redis_url = ENV["REDIS_URL"]
+
+# db > 1 does not work for old redis service, but when we will switch to paas redis it should work
+# redis_db = ENV["REDIS_DB"] || 0
+redis_db = 0
 
 redis_config = { url: redis_url, db: redis_db }
 
