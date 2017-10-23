3a3e4772ac998d1c68c1353d302484d24990b94c
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-10-23 19:31:31 +0300

    [redis] enable redis-alpha for dev and staging

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,17 @@
 require "sidekiq"
 
-# PASS Redis is not ready for prod
-# redis_url = begin
-#   JSON.parse(ENV["VCAP_SERVICES"])["user-provided"].select{ |s| s["name"] == "redis-alpha" }[0]["credentials"]["uri"]
-# rescue
-#   ENV["REDIS_URL"]
-# end
+# PaaS Redis is not ready for prod
+redis_url = if Rails.env.production?
+  ENV["REDIS_URL"]
+else
+  begin
+    JSON.parse(ENV["VCAP_SERVICES"])["user-provided"].select{ |s| s["name"] == "redis-alpha" }[0]["credentials"]["uri"]
+  rescue
+    ENV["REDIS_URL"]
+  end
+end
 
-redis_url = ENV["REDIS_URL"]
-
-# db > 1 does not work for old redis service, but when we will switch to paas redis it should work
+# db > 1 does not work for old redis service, but when we will switch to PaaS redis it should work
 # redis_db = ENV["REDIS_DB"] || 0
 redis_db = 0
 
