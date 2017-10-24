9e3a1656b0b5aabe13e432f6fc00bf3ef9ef0c71
Mario Andrés Correa <a@macool.me>
2017-10-24 18:55:53 -0500

    mapping for AdditionalCodeDescriptionPeriod

---
+++ lib/cds_importer/entity_mapper/additional_code_description_period_mapper.rb
@@ -0,0 +1,18 @@
+class CdsImporter
+  class EntityMapper
+    class AdditionalCodeDescriptionPeriodMapper < BaseMapper
+      self.entity_class = "AdditionalCodeDescriptionPeriod".freeze
+
+      self.mapping_path = "additionalCodeDescriptionPeriod".freeze
+
+      self.exclude_mapping = ["metainfo.origin"]
+
+      self.entity_mapping = base_mapping.merge(
+        "additionalCodeDescriptionPeriod.sid" => :additional_code_description_period_sid,
+        "sid" => :additional_code_sid,
+        "additionalCodeDescriptionPeriod.additionalCodeType.additionalCodeTypeId" => :additional_code_type_id,
+        "additionalCodeCode" => :additional_code
+      ).freeze
+    end
+  end
+end
--- spec/integration/cds_importer/entity_mapper_spec.rb
+++ spec/integration/cds_importer/entity_mapper_spec.rb
@@ -81,5 +81,38 @@ describe CdsImporter::EntityMapper do
       expect(entity.operation).to eq(:create)
       expect(entity.operation_date).to eq(Date.parse(hash_double.values["additionalCodeDescriptionPeriod"]["additionalCodeDescription"]["metainfo"]["transactionDate"]))
     end
+
+    it "AdditionalCodeDescriptionPeriod sample" do
+      hash_double = double(
+        :hash_from_node,
+        name: "AdditionalCodeDescriptionPeriod",
+        values: {
+          "sid" => 3084,
+          "additionalCodeCode" => "169",
+          "additionalCodeDescriptionPeriod" => {
+            "sid" => 536,
+            "validityStartDate" => "1991-06-01T00:00:00",
+            "validityEndDate" => "1992-06-01T00:00:00",
+            "additionalCodeType" => {
+              "additionalCodeTypeId" => "8"
+            },
+            "metainfo" => {
+              "opType" => "U",
+              "transactionDate" => "2016-07-27T09:20:15"
+            }
+          }
+        }
+      )
+      klass = CdsImporter::EntityMapper::AdditionalCodeDescriptionPeriodMapper
+      entity = klass.new(hash_double.values).parse
+      expect(entity.additional_code_description_period_sid).to eq(hash_double.values["additionalCodeDescriptionPeriod"]["sid"])
+      expect(entity.additional_code_sid).to eq(hash_double.values["sid"])
+      expect(entity.additional_code_type_id).to eq(hash_double.values["additionalCodeDescriptionPeriod"]["additionalCodeType"]["additionalCodeTypeId"])
+      expect(entity.additional_code).to eq(hash_double.values["additionalCodeCode"])
+      expect(entity.validity_start_date).to eq(hash_double.values["additionalCodeDescriptionPeriod"]["validityStartDate"])
+      expect(entity.validity_end_date).to eq(hash_double.values["additionalCodeDescriptionPeriod"]["validityEndDate"])
+      expect(entity.operation).to eq(:update)
+      expect(entity.operation_date).to eq(Date.parse(hash_double.values["additionalCodeDescriptionPeriod"]["metainfo"]["transactionDate"]))
+    end
   end
 end
