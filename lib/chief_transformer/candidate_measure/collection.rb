48f8af503a90bd6b7584653dcbd722483cdfaa0a
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-12-13 00:45:18 +0300

    add measures logging for created and failed candidate records

--- lib/chief_transformer/candidate_measure/collection.rb
+++ lib/chief_transformer/candidate_measure/collection.rb
@@ -13,32 +13,43 @@ class ChiefTransformer
 
       def uniq
         # Remove duplicates
-        @measures = @measures.uniq {|m|
-          [m.measure_type_id, m.geographical_area_id, m.goods_nomenclature_item_id,
-           m.additional_code_type_id, m.additional_code_id, m.validity_start_date,
-           m.validity_end_date].join
+        @measures = @measures.uniq{ |m|
+          [
+            m.measure_type_id,
+            m.geographical_area_id,
+            m.goods_nomenclature_item_id,
+            m.additional_code_type_id,
+            m.additional_code_id,
+            m.validity_start_date,
+            m.validity_end_date
+          ].join
         }
       end
 
       def sort
-        @measures = @measures.sort_by {|m|
+        @measures = @measures.sort_by{|m|
           m.validity_end_date.to_s
         }.reverse
       end
 
       def validate
         # This measure does not 'cover' any time frame and wastes space instead
-        @measures = @measures.reject{|m| m.validity_start_date == m.validity_end_date }
+        @measures = @measures.reject{ |m| m.validity_start_date == m.validity_end_date }
 
         # remove obviously invalid measures
-        @measures = @measures.reject { |m| m.validity_end_date.present? &&
-                                           m.validity_start_date > m.validity_end_date }
+        @measures = @measures.reject{ |m| m.validity_end_date.present? &&
+                                          m.validity_start_date > m.validity_end_date }
       end
 
       def persist
         Sequel::Model.db.transaction do
           @measures.each do |candidate_measure|
-            candidate_measure.save if candidate_measure.valid?
+            if candidate_measure.valid?
+              candidate_measure.save
+              ChiefTransformer::MeasuresLogger.add_created(candidate_measure)
+            else
+              ChiefTransformer::MeasuresLogger.add_failed(candidate_measure)
+            end
           end
         end
       end
