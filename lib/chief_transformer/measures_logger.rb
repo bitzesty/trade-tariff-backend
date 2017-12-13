2bd5cb1de4056a76261e2476c8920a6453b3e429
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-12-13 17:35:45 +0300

    make measure log files machine readable, skip updated and deleted measures

--- lib/chief_transformer/measures_logger.rb
+++ lib/chief_transformer/measures_logger.rb
@@ -1,3 +1,5 @@
+# each line in *.json.txt files is a json object of measure candidate
+
 class ChiefTransformer
   class MeasuresLogger
     class << self
@@ -5,7 +7,7 @@ class ChiefTransformer
         return unless TariffSynchronizer.measures_logger_enabled
 
         File.open(tmp_file_path(:created), "a+") do |f|
-          f.puts("#{Time.now} Measure Candidate: #{candidate.values.inspect}")
+          f.puts(candidate.values.to_json)
         end
       end
 
@@ -13,8 +15,7 @@ class ChiefTransformer
         return unless TariffSynchronizer.measures_logger_enabled
 
         File.open(tmp_file_path(:failed), "a+") do |f|
-          f.puts("#{Time.now} Measure Candidate: #{candidate.values.inspect}")
-          f.puts("Errors: #{candidate.errors}")
+          f.puts(candidate.values.merge(errors: candidate.errors).to_json)
         end
       end
 
@@ -22,13 +23,13 @@ class ChiefTransformer
         return unless TariffSynchronizer.measures_logger_enabled
 
         [:created, :failed].each do |type|
-          path = File.join(TariffSynchronizer.root_path, "measures", "#{Date.today}-#{type}.txt")
+          path = File.join(TariffSynchronizer.root_path, "measures", "#{Date.today}-#{type}.json.txt")
           TariffSynchronizer::FileService.upload_file(tmp_file_path(type), path)
         end
       end
 
       def tmp_file_path(type)
-        File.join(Rails.root, "data", "measures", "#{Date.today}-#{type}.txt")
+        File.join(Rails.root, "data", "measures", "#{Date.today}-#{type}.json.txt")
       end
     end
   end
