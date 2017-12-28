9a3ace810d3e8d6a02806602939dd0c8a4203682
Alexey Klimuk <alexey.klimuk@gmail.com>
2017-12-28 15:59:33 +0300

    adding PublicationSigle model, table and view

---
+++ app/models/publication_sigle.rb
@@ -0,0 +1,12 @@
+class PublicationSigle < Sequel::Model
+  plugin :time_machine
+  plugin :oplog, primary_key: [:oid, :code, :code_type_id]
+  plugin :national
+  plugin :conformance_validator
+
+  set_primary_key [:code, :code_type_id]
+
+  def national?
+    national
+  end
+end
