cecbd9b4a55a5d3d96d521877cdb2ef3e21af363
Ruslan Khamidullin <rusllonrails@gmail.com>
2018-01-19 15:17:30 +0300

    [MEASURE] Council Regulation - URL generation improvements
    **DESCRIPTION:**
    
    We need to update the URL so that it finds the correct url, attached screenshots show one format and this is another from the client with an example:
    
    https://www.trade-tariff.service.gov.uk/trade-tariff/commodities/8711601000?country=CN#import
    
    The hyperlink on the pages above to 1053/17 does not work, I think it should go to I0353/17
    
    [TRELLO CARD](https://trello.com/c/J4Ddzqgr/453-tariff17-improve-lookup-of-legal-notices)

--- app/models/measure_partial_temporary_stop.rb
+++ app/models/measure_partial_temporary_stop.rb
@@ -6,6 +6,9 @@ class MeasurePartialTemporaryStop < Sequel::Model
 
   set_primary_key [:measure_sid, :partial_temporary_stop_regulation_id]
 
+  alias_attribute :officialjournal_number, :partial_temporary_stop_regulation_officialjournal_number
+  alias_attribute :officialjournal_page, :partial_temporary_stop_regulation_officialjournal_page
+
   def regulation_id
     partial_temporary_stop_regulation_id
   end
