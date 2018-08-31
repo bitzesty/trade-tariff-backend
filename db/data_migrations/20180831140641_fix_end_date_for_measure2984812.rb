b8d809b178bc3616de3aea856702f655fe24d8fe
Alexey Klimuk <alexey.klimuk@gmail.com>
2018-08-31 14:19:54 +0200

    Add data migration to fix measure 2984812 end date as it's different on EU TARIC

---
+++ db/data_migrations/20180831140641_fix_end_date_for_measure2984812.rb
@@ -0,0 +1,27 @@
+TradeTariffBackend::DataMigrator.migration do
+  name "Set end date for measure sid 2984812 as we have discrepancy in data between the Trade Tariff" \
+        "and EU's Taric consultation. See 1703100000."
+
+  MEASURE_SID = 2984812
+  VALIDITY_END_DATE = Date.new(2008, 9, 30)
+
+  up do
+    applicable {
+      true
+    }
+    apply {
+      operation = Measure::Operation.where(measure_sid: MEASURE_SID).last
+      operation.update(validity_end_date: VALIDITY_END_DATE)
+    }
+  end
+
+  down do
+    applicable {
+      true
+    }
+    apply {
+      operation = Measure::Operation.where(measure_sid: MEASURE_SID).last
+      operation.update(validity_end_date: nil)
+    }
+  end
+end
