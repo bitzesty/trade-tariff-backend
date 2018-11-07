e977aa192249655c774ece500b68ae0ac003da4d
Alexey Klimuk <alexey.klimuk@gmail.com>
2018-11-07 10:46:56 +0100

    [redis] Use paas redis service

--- lib/redis_resolver.rb
+++ lib/redis_resolver.rb
@@ -2,18 +2,14 @@ module RedisResolver
   module_function
 
   def get_redis_config
-    # PaaS Redis is not ready for prod
-    redis_url = if TradeTariffBackend.production?
+    redis_url = begin
+      # TODO: !Important
+      # need to fetch by service name if we use multiple redis services
+      JSON.parse(ENV["VCAP_SERVICES"])["redis"][0]["credentials"]["uri"]
+    rescue
       ENV["REDIS_URL"]
-    else
-      begin
-        # TODO: !Important
-        # need to fetch by service name if we use multiple redis services
-        JSON.parse(ENV["VCAP_SERVICES"])["redis"][0]["credentials"]["uri"]
-      rescue
-        ENV["REDIS_URL"]
-      end
     end
+
     { url: redis_url, db: 0 }
   end
 end
