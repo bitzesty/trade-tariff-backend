f3c439059bb1e60d2aed18ad84e29f173f2b80b3
Alexey Klimuk <alexey.klimuk@gmail.com>
2018-11-26 14:30:34 +0100

    Fix Quota factory after cherry-pick

--- spec/factories/quota_factory.rb
+++ spec/factories/quota_factory.rb
@@ -1,11 +1,63 @@
 FactoryGirl.define do
   sequence(:quota_order_number_sid) { |n| n}
+  sequence(:quota_order_number_id) do
+    "09" + Forgery(:basic).number(at_least: 1000, at_most: 9999).to_s
+  end
+
+  factory :quota_association do
+    main_quota_definition_sid  { Forgery(:basic).number }
+    sub_quota_definition_sid   { Forgery(:basic).number }
+    relation_type              { Forgery(:basic).text(exactly: 2) }
+    coefficient                { Forgery(:basic).number }
+  end
 
   factory :quota_order_number do
     quota_order_number_sid { generate(:quota_order_number_sid) }
-    quota_order_number_id  { 6.times.map{ Random.rand(9) }.join }
+    quota_order_number_id  { generate(:quota_order_number_id) }
     validity_start_date { Date.today.ago(4.years) }
     validity_end_date   { nil }
+
+    trait :xml do
+      validity_end_date       { Date.today.ago(1.years) }
+    end
+  end
+
+  factory :quota_order_number_origin do
+    quota_order_number_origin_sid  { generate(:sid) }
+    quota_order_number_sid         { generate(:sid) }
+    geographical_area_id           { Forgery(:basic).text(exactly: 2) }
+    geographical_area_sid          { generate(:sid) }
+    validity_start_date            { Date.today.ago(4.years) }
+    validity_end_date              { nil }
+
+    trait :xml do
+      validity_end_date              { Date.today.ago(1.years) }
+    end
+
+    trait :with_geographical_area do
+      after(:build) do |qon|
+        geographical_area = create(:geographical_area)
+        qon.geographical_area_id = geographical_area.geographical_area_id
+        qon.geographical_area_sid = geographical_area.geographical_area_sid
+      end
+    end
+  end
+
+  factory :quota_order_number_origin_exclusion do
+    quota_order_number_origin_sid    { generate(:sid) }
+
+    after(:build) do |exclusion|
+      if exclusion.excluded_geographical_area_sid.blank?
+        geographical_area = create(:geographical_area)
+        exclusion.excluded_geographical_area_sid = geographical_area.geographical_area_sid
+      end
+    end
+  end
+
+  factory :quota_reopening_event do
+    quota_definition_sid  { generate(:sid) }
+    occurrence_timestamp  { 24.hours.ago }
+    reopening_date        { Date.today.ago(1.years) }
   end
 
   factory :quota_definition do
@@ -51,19 +103,42 @@ FactoryGirl.define do
     old_balance { Forgery(:basic).number }
     new_balance { Forgery(:basic).number }
     imported_amount { Forgery(:basic).number }
-    occurrence_timestamp { Time.now }
+    occurrence_timestamp { 24.hours.ago }
   end
 
   factory :quota_exhaustion_event do
     quota_definition
     exhaustion_date { Date.today }
-    occurrence_timestamp { Time.now }
+    occurrence_timestamp { 24.hours.ago }
   end
 
   factory :quota_critical_event do
     quota_definition
     critical_state_change_date { Date.today }
-    occurrence_timestamp { Time.now }
+    occurrence_timestamp       { 24.hours.ago }
+
+    trait :xml do
+      critical_state           { Forgery(:basic).text(exactly: 2) }
+    end
   end
 
+  factory :quota_suspension_period do
+    quota_suspension_period_sid  { generate(:sid) }
+    quota_definition_sid         { generate(:sid) }
+    suspension_start_date        { Date.today.ago(1.years) }
+    suspension_end_date          { Date.today.ago(1.years) }
+    description                  { Forgery(:lorem_ipsum).sentence }
+  end
+
+  factory :quota_unblocking_event do
+    quota_definition_sid  { generate(:sid) }
+    occurrence_timestamp  { 24.hours.ago }
+    unblocking_date       { Date.today.ago(1.years) }
+  end
+
+  factory :quota_unsuspension_event do
+    quota_definition_sid  { generate(:sid) }
+    occurrence_timestamp  { 24.hours.ago }
+    unsuspension_date     { Date.today.ago(1.years) }
+  end
 end
