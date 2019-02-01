bd8ae3d781672a44d601156d34dfc47dd762ff11
Ryan MacGillivray <rmacg89@gmail.com>
2019-02-01 10:08:15 +0000

    Revert "Merge branch 'commodity-measures-search-index' into 'master'"
    This reverts merge request !354

--- app/search_indexes/commodity_index.rb
+++ app/search_indexes/commodity_index.rb
@@ -48,29 +48,8 @@ class CommodityIndex < SearchIndex
             overview_measures: {
               dynamic: true,
               properties: {
-                goods_nomenclature_sid: { type: "keyword" },
-                additional_code: { type: "nested" },
                 measure_sid: { type: "long" },
                 measure_type_id: { type: "keyword" },
-                vat?: { type: "boolean" },
-                third_country?: { type: "boolean" },
-                measure_type: {
-                  type: "nested",
-                  properties: {
-                    description: { type: "text" },
-                  }
-                },
-                duty_expression_with_national_measurement_units_for: { type: "text" },
-                formatted_duty_expression_with_national_measurement_units_for: { type: "text" },
-                measure_components: {
-                  type: "nested",
-                  properties: {
-                    duty_expression: { type: "text" },
-                    measurement_unit: { type: "nested" },
-                    monetary_unit: { type: "nested" },
-                    formatted_duty_expression: { type: "text" }
-                  }
-                },
                 effective_start_date: { format: "dateOptionalTime", type: "date" },
                 effective_end_date: { format: "dateOptionalTime", type: "date" },
               }
--- app/services/commodity_service/overview_measures_service.rb
+++ app/services/commodity_service/overview_measures_service.rb
@@ -1,28 +1,29 @@
 module CommodityService
   class OverviewMeasuresService
+
     def initialize(goods_nomenclature_sid, as_of)
       @goods_nomenclature_sid = goods_nomenclature_sid
       @as_of = as_of
     end
 
-    def indexed_measures
-      measures = commodity&.overview_measures&.select do |measure|
+    def measure_sids
+      commodity&.overview_measures&.select do |measure|
         if @as_of.present?
           measure.effective_start_date.to_date <= @as_of &&
-            (measure.effective_end_date.nil? || measure.effective_end_date.to_date >= @as_of)
+              (measure.effective_end_date == nil || measure.effective_end_date.to_date >= @as_of)
         else
           true
         end
-      end&.map do |effective_measure|
-        effective_measure
+      end&.map do |measure|
+        measure.measure_sid
       end
-      measures || []
     end
 
     def commodity
       search_client = ::TradeTariffBackend.search_client
-      result = search_client.search index: CommodityIndex.new(TradeTariffBackend.search_namespace).name, body: { query: { match: { id: @goods_nomenclature_sid } } }
+      result = search_client.search index: CommodityIndex.new(TradeTariffBackend.search_namespace).name, body: {query: {match: {id: @goods_nomenclature_sid}}}
       result&.hits&.hits&.first&._source
     end
+
   end
 end
