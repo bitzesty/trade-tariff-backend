93639e5a1fe6827d4389df281c4642b6a0b9cfba
Ryan MacGillivray <rmacg89@gmail.com>
2019-03-04 15:36:14 +0000

    Update migration to remove more incorrect measures
    After solving the last batch of zero duty measures we discovered more
    had been addied prior to deploying the migration as such we've removed
    them in the existing migration.

--- db/data_migrations/20190228161327_resolve_zero_percent_measures_on_cigarettes.rb
+++ db/data_migrations/20190228161327_resolve_zero_percent_measures_on_cigarettes.rb
@@ -4,14 +4,20 @@ TradeTariffBackend::DataMigrator.migration do
   up do
     applicable do
       Measure::Operation.where(measure_sid: -499434, measure_type_id: 'FAA', goods_nomenclature_item_id: '2402201000').any?
+      Measure::Operation.where(measure_sid: -499676, measure_type_id: 'FAA', goods_nomenclature_item_id: '2402201000').any?
+      Measure::Operation.where(measure_sid: -513959, measure_type_id: 'FAA', goods_nomenclature_item_id: '2402201000').any?
+      Measure::Operation.where(measure_sid: -500832, measure_type_id: 'FAA', goods_nomenclature_item_id: '2402900000').any?
       Measure::Operation.where(measure_sid: -501457, measure_type_id: 'FAA', goods_nomenclature_item_id: '2402900000').any?
-      # -501457 2402900000
-      # -499434 2402201000
+      Measure::Operation.where(measure_sid: -512829, measure_type_id: 'FAA', goods_nomenclature_item_id: '2402900000').any?
     end
 
     apply do
       Measure::Operation.where(measure_sid: -499434, measure_type_id: 'FAA', goods_nomenclature_item_id: '2402201000').delete
+      Measure::Operation.where(measure_sid: -499676, measure_type_id: 'FAA', goods_nomenclature_item_id: '2402201000').delete
+      Measure::Operation.where(measure_sid: -513959, measure_type_id: 'FAA', goods_nomenclature_item_id: '2402201000').delete
+      Measure::Operation.where(measure_sid: -500832, measure_type_id: 'FAA', goods_nomenclature_item_id: '2402900000').delete
       Measure::Operation.where(measure_sid: -501457, measure_type_id: 'FAA', goods_nomenclature_item_id: '2402900000').delete
+      Measure::Operation.where(measure_sid: -512829, measure_type_id: 'FAA', goods_nomenclature_item_id: '2402900000').delete
       Measure::Operation.where(measure_sid: -490646).update(validity_end_date: nil)
       Measure::Operation.where(measure_sid: -490647).update(validity_end_date: nil)
     end
