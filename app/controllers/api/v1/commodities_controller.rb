fb2b7692429034b9c9057d9f6e41ad383a2e8c06
Matthew Ford <matt@bitzesty.com>
2019-03-07 20:23:31 +0000

    Remove national ordering of measures as all measures are national
    Going forward all measures will be national, this behaviour is inconsistent with the headings controller and should be done in the model anyway

--- app/controllers/api/v1/commodities_controller.rb
+++ app/controllers/api/v1/commodities_controller.rb
@@ -32,8 +32,6 @@ module Api
             :additional_code,
             :full_temporary_stop_regulations,
             :measure_partial_temporary_stops
-          ).order(
-            Sequel.asc(:measures__national, nulls: :last), Sequel.asc(:measures__geographical_area_id)
           ).all, @commodity
         ).validate!
 
