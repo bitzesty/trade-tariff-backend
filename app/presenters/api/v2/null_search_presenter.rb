cb06aafd6d6310cb3a59a663bfad13f13a8e95b4
myxrome <myxrome@gmail.com>
2019-05-22 18:20:54 +0400

    fixing code quality issues.

--- app/presenters/api/v2/null_search_presenter.rb
+++ app/presenters/api/v2/null_search_presenter.rb
@@ -1,26 +1,8 @@
 module Api
   module V2
-    class NullSearchPresenter
-      attr_reader :result
-  
-      def initialize(result)
-        @result = result
-      end
-  
-      def id
-        1
-      end
-  
+    class NullSearchPresenter < ::Api::V2::FuzzySearchPresenter
       def type
         'null_match'
-      end
-  
-      def goods_nomenclature_match
-        result.serializable_hash[:goods_nomenclature_match]
-      end
-  
-      def reference_match
-        result.serializable_hash[:reference_match]
       end
     end
   end
