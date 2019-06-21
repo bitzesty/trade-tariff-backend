c4f96013bb0c5d927765016b89bc697ca81b5e49
Christopher Unger <christopher.unger@bitzesty.com>
2019-06-21 16:49:29 +0000

    Update application_controller_spec.rb, fix typos

--- spec/requests/application_controller_spec.rb
+++ spec/requests/application_controller_spec.rb
@@ -1,19 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe "get a 404 response (in JSON) from unmatched route", :type => :request do
-  it 'when the request id for HTML' do
+  it 'when the request is for HTML' do
     get '/foo'
     expect(response.body).to eq('{"error":"404 - Not Found"}')
     expect(response).to have_http_status(:not_found)
   end
   
-  it 'when the request id for CSV' do
+  it 'when the request is for CSV' do
     get '/foo', headers: {"Content-Type": "text/csv"}
     expect(response.body).to eq('{"error":"404 - Not Found"}')
     expect(response).to have_http_status(:not_found)
   end
 
-  it 'when the request id for JSON' do
+  it 'when the request is for JSON' do
     get '/foo', headers: {"Content-Type": "application/vnd.uktt.v2"}
     expect(response.body).to eq('{"error":"404 - Not Found"}')
     expect(response).to have_http_status(:not_found)
