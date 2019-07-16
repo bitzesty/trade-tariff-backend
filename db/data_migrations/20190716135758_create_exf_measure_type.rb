7c1bd972d2b715877c7949030cae0f75de6bad20
myxrome <myxrome@gmail.com>
2019-07-16 17:22:02 +0400

    added explanation in the comment.

--- db/data_migrations/20190716135758_create_exf_measure_type.rb
+++ db/data_migrations/20190716135758_create_exf_measure_type.rb
@@ -1,3 +1,14 @@
+=begin
+CHIEF transformer do not handle TAMT (measure type) and RVTT (measure type description)
+record types, so we created these records manually.
+
+2019-01-10_KBT009(19010).txt:2
+"TAMT       ","09/01/2019:10:37:00","I","487","EX","EXF",null,null,"09/01/2019:10:36:00",
+
+2018-12-28_KBT009(18362).txt:4987
+"RVTT       ","27/12/2018:10:27:00","I","487",null,null,"27/12/2018:10:25:00",null,"N",null,null,"4","N","Y","CIDER AND PERRY EXCEEDING 6.9% - NOT EXCEEDING 7.5% ABV","Y","Y","E",
+=end
+
 TradeTariffBackend::DataMigrator.migration do
   name 'Create EXF national measure type with description'
 
