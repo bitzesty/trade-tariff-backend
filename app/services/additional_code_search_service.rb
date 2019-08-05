2f2138ea0011a9f86ebd8165078eefd9c78a009d
Aleksey Zolotov <myxrome@gmail.com>
2019-08-05 16:57:51 +0400

    additional code search service refactoring.

--- app/services/additional_code_search_service.rb
+++ app/services/additional_code_search_service.rb
@@ -1,31 +1,35 @@
 class AdditionalCodeSearchService
-  attr_reader :scope
+  attr_accessor :scope
+  attr_reader :code, :description
 
   def initialize(attributes)
     @scope = AdditionalCode.
         actual.
         eager(:additional_code_descriptions)
 
-    if attributes.present?
-      attributes.each do |name, value|
-        send(:"#{name}=", value) if self.respond_to?(:"#{name}=") && value.present?
-      end
-    end
-  end
-
-  def code=(value)
-    @scope = scope.where(additional_codes__additional_code: value)
-  end
-
-  def description=(value)
-    @scope = scope.
-        join(:additional_code_description_periods, additional_codes__additional_code_sid: :additional_code_description_periods__additional_code_sid).
-        join(:additional_code_descriptions, [[:additional_code_description_periods__additional_code_description_period_sid, :additional_code_descriptions__additional_code_description_period_sid]]).
-        with_actual(AdditionalCodeDescriptionPeriod).
-        where(Sequel.ilike(:additional_code_descriptions__description, "#{value}%"))
+    @code = attributes['code']
+    @description = attributes['description']
   end
 
   def perform
+    apply_code_filter
+    apply_description_filter
     scope.all
+  end
+
+  private
+
+  def apply_code_filter
+    return if code.blank?
+    self.scope = scope.where(additional_codes__additional_code: code)
+  end
+
+  def apply_description_filter
+    return if description.blank?
+    self.scope = scope.
+      join(:additional_code_description_periods, additional_codes__additional_code_sid: :additional_code_description_periods__additional_code_sid).
+      join(:additional_code_descriptions, [[:additional_code_description_periods__additional_code_description_period_sid, :additional_code_descriptions__additional_code_description_period_sid]]).
+      with_actual(AdditionalCodeDescriptionPeriod).
+      where(Sequel.ilike(:additional_code_descriptions__description, "#{description}%"))
   end
 end
