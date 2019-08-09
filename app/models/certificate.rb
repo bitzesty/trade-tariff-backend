50089ab86aecdd4f3a2aff7443e71877a4291bb5
Aleksey Zolotov <myxrome@gmail.com>
2019-08-09 14:39:39 +0400

    reverted description calculation changes.

--- app/models/certificate.rb
+++ app/models/certificate.rb
@@ -29,7 +29,7 @@ class Certificate < Sequel::Model
   end
 
   def certificate_description
-    certificate_descriptions(reload: true).last
+    certificate_descriptions(reload: true).first
   end
 
   one_to_many :certificate_types, key: :certificate_type_code,
