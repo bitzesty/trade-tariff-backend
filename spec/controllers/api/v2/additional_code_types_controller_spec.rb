3557609c70d4c87414195731b135f70e1c6d87ba
Aleksey Zolotov <myxrome@gmail.com>
2019-08-14 16:26:02 +0400

    added specs for new endpoint and search filter.

---
+++ spec/controllers/api/v2/additional_code_types_controller_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe Api::V2::AdditionalCodeTypesController, type: :controller do
+  render_views
+
+  context '#index' do
+
+    let!(:additional_code_type_1) { create :additional_code_type }
+    let!(:additional_code_type_description_1) { create :additional_code_type_description, additional_code_type_id: additional_code_type_1.additional_code_type_id }
+    let!(:additional_code_type_2) { create :additional_code_type }
+    let!(:additional_code_type_description_2) { create :additional_code_type_description, additional_code_type_id: additional_code_type_2.additional_code_type_id }
+    let!(:additional_code_type_3) { create :additional_code_type }
+    let!(:additional_code_type_description_3) { create :additional_code_type_description, additional_code_type_id: additional_code_type_3.additional_code_type_id }
+
+    let(:pattern) {
+      {
+        "data": [{
+          "id": String,
+          "type": "additional_code_type",
+          "attributes": {
+            "additional_code_type_id": String,
+            "description": String
+          }
+        }, {
+          "id": String,
+          "type": "additional_code_type",
+          "attributes": {
+            "additional_code_type_id": String,
+            "description": String
+          }
+        }, {
+          "id": String,
+          "type": "additional_code_type",
+          "attributes": {
+            "additional_code_type_id": String,
+            "description": String
+          }
+        }]
+      }
+    }
+
+    it 'returns rendered found additional codes and related measures and goods nomenclatures' do
+      get :index, format: :json
+
+      expect(response.body).to match_json_expression pattern
+    end
+  end
+end 
