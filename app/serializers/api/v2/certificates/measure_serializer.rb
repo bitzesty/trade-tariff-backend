2c942df13e6829d8d2d46f7f0dd894f0218d5c27
Aleksey Zolotov <myxrome@gmail.com>
2019-09-30 19:33:27 +0400

    added `goods_nomenclature_item_id` for measures in searched entities.

--- app/serializers/api/v2/certificates/measure_serializer.rb
+++ app/serializers/api/v2/certificates/measure_serializer.rb
@@ -8,7 +8,7 @@ module Api
 
         set_id :measure_sid
 
-        attributes :id, :validity_start_date, :validity_end_date
+        attributes :id, :validity_start_date, :validity_end_date, :goods_nomenclature_item_id
 
         has_one :goods_nomenclature, id_method_name: :goods_nomenclature_sid, serializer: Api::V2::Certificates::GoodsNomenclatureSerializer
       end
