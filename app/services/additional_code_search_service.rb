5e93a901c7e2a77643d04bc3249d56a2a0f0c9a7
Aleksey Zolotov <myxrome@gmail.com>
2019-10-10 17:09:38 +0400

    reimplemented additional code search.

--- app/services/additional_code_search_service.rb
+++ app/services/additional_code_search_service.rb
@@ -1,14 +1,43 @@
 class AdditionalCodeSearchService
-  attr_accessor :scope
-  attr_reader :code, :type, :description, :current_page, :per_page
-
-  delegate :pagination_record_count, to: :scope
+  attr_reader :code, :type, :description, :as_of
+  attr_reader :current_page, :per_page, :pagination_record_count
 
   def initialize(attributes, current_page, per_page)
-    self.scope = AdditionalCode.
-        actual.
-        eager(:additional_code_descriptions).
-        order(:additional_codes__additional_code_type_id, :additional_codes__additional_code)
+    @as_of = AdditionalCode.point_in_time
+    @query = [{
+      bool: {
+        should: [
+          # actual date is either between item's (validity_start_date..validity_end_date)
+          {
+            bool: {
+              must: [
+                { range: { validity_start_date: { lte: as_of } } },
+                { range: { validity_end_date: { gte: as_of } } }
+              ]
+            }
+          },
+          # or is greater than item's validity_start_date
+          # and item has blank validity_end_date (is unbounded)
+          {
+            bool: {
+              must: [
+                { range: { validity_start_date: { lte: as_of } } },
+                { bool: { must_not: { exists: { field: "validity_end_date" } } } }
+              ]
+            }
+          },
+          # or item has blank validity_start_date and validity_end_date
+          {
+            bool: {
+              must: [
+                { bool: { must_not: { exists: { field: "validity_start_date" } } } },
+                { bool: { must_not: { exists: { field: "validity_end_date" } } } }
+              ]
+            }
+          }
+        ]
+      }
+    }]
 
     @code = attributes['code']
     @code = @code[1..-1] if @code&.length == 4
@@ -16,31 +45,47 @@ class AdditionalCodeSearchService
     @description = attributes['description']
     @current_page = current_page
     @per_page = per_page
+    @pagination_record_count = 0
   end
 
   def perform
     apply_code_filter if code.present?
     apply_type_filter if type.present?
     apply_description_filter if description.present?
-    self.scope = scope.paginate(current_page, per_page)
-    scope.to_a
+    fetch
+    filter_measures if @result.present?
+    @result
   end
 
   private
 
+  def fetch
+    search_client = ::TradeTariffBackend.search_client
+    index = ::Cache::AdditionalCodeIndex.new(TradeTariffBackend.search_namespace).name
+    result = search_client.search index: index, body: { query: { constant_score: { filter: { bool: { must: @query } } } }, size: per_page, from: (current_page - 1) * per_page, sort: %w(additional_code_type_id additional_code) }
+    @pagination_record_count = result&.hits&.total || 0
+    @result = result&.hits&.hits&.map(&:_source)
+  end
+
+  def filter_measures
+    @result.each do |additional_code|
+      additional_code.measures.keep_if do |measure|
+        measure.validity_start_date.to_date <= as_of &&
+          (measure.validity_end_date.nil? || measure.validity_end_date.to_date >= as_of)
+      end
+      additional_code.measure_ids = additional_code.measures.map(&:id)
+    end
+  end
+
   def apply_code_filter
-    self.scope = scope.where(additional_codes__additional_code: code)
+    @query.push({ bool: { must: { term: { additional_code: code } } } })
   end
 
   def apply_type_filter
-    self.scope = scope.where(additional_codes__additional_code_type_id: type)
+    @query.push({ bool: { must: { term: { additional_code_type_id: type } } } })
   end
 
   def apply_description_filter
-    self.scope = scope.
-      join(:additional_code_description_periods, additional_codes__additional_code_sid: :additional_code_description_periods__additional_code_sid).
-      join(:additional_code_descriptions, [[:additional_code_description_periods__additional_code_description_period_sid, :additional_code_descriptions__additional_code_description_period_sid]]).
-      with_actual(AdditionalCodeDescriptionPeriod).
-      where(Sequel.ilike(:additional_code_descriptions__description, "#{description}%"))
+    @query.push({ multi_match: { query: description, fields: %w[description], operator: 'and' } })
   end
 end
