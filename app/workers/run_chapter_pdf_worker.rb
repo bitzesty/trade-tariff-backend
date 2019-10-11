b4f643f28566aefd3b4843a397f6481f0e4c3cbd
Christopher Unger <mcu@mac.com>
2019-10-11 08:16:41 -0400

    Do not look up the exchange rate for EUR (it's always 1.0!)

--- app/workers/run_chapter_pdf_worker.rb
+++ app/workers/run_chapter_pdf_worker.rb
@@ -11,7 +11,11 @@ class RunChapterPdfWorker
       batch.description = "Produces PDFs for all chapters (in #{currency})"
       chapter_ids = Section.all.map(&:chapters).flatten.map(&:goods_nomenclature_sid)
       # chapter_ids = %w[47137 32338 54748] # <- short chapters
-      ENV["MX_RATE_EUR_#{currency}"] ||= MonetaryExchangeRate.latest(currency).to_s
+      if currency === 'EUR'
+        ENV["MX_RATE_EUR_EUR"] = '1.0'
+      else
+        ENV["MX_RATE_EUR_#{currency}"] ||= MonetaryExchangeRate.latest(currency).to_s
+      end
       if chapter_ids.empty?
         puts "Cancelled batch #{batch.bid}. No chapters were specified."
       else
