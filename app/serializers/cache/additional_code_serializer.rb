b6b748658c72ad445a32cdec43c51f0f3b328e1b
Aleksey Zolotov <myxrome@gmail.com>
2019-10-21 14:54:10 +0400

    fixed failed specs.

--- app/serializers/cache/additional_code_serializer.rb
+++ app/serializers/cache/additional_code_serializer.rb
@@ -21,16 +21,16 @@ module Cache
         description: additional_code.description,
         formatted_description: additional_code.formatted_description,
         validity_start_date: additional_code.validity_start_date,
-        effective_start_date: additional_code.effective_start_date,
         validity_end_date: additional_code.validity_end_date,
-        effective_end_date: additional_code.effective_end_date,
         measure_ids: measures.map(&:measure_sid),
         measures: measures.map do |measure|
           {
             id: measure.measure_sid,
             measure_sid: measure.measure_sid,
             validity_start_date: measure.validity_start_date,
+            effective_start_date: measure.effective_start_date,
             validity_end_date: measure.validity_end_date,
+            effective_end_date: measure.effective_end_date,
             goods_nomenclature_item_id: measure.goods_nomenclature_item_id,
             goods_nomenclature_sid: measure.goods_nomenclature_sid,
             goods_nomenclature: goods_nomenclature_attributes(measure.goods_nomenclature)
