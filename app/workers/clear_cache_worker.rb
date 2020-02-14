54fe70a5f98361f2dfdb943f07c2fbb7815e2170
Matthew Ford <matt@bitzesty.com>
2020-02-14 15:26:53 +0000

    Add ClearCacheWorker and run it at midnight

---
+++ app/workers/clear_cache_worker.rb
@@ -0,0 +1,12 @@
+class ClearCacheWorker
+    include Sidekiq::Worker
+  
+    sidekiq_options retry: false
+  
+    def perform
+      logger.info "Clearing Rails cache"
+      Rails.cache.clear
+      logger.info "Clearing Rails cache completed"
+    end
+  end
+end
