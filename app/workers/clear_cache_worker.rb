5e99a8b08d7ddd6f70f844352617dc7898610e92
Matthew Ford <matt@bitzesty.com>
2020-02-14 22:18:05 +0000

    Typo

--- app/workers/clear_cache_worker.rb
+++ app/workers/clear_cache_worker.rb
@@ -1,12 +1,11 @@
 class ClearCacheWorker
-    include Sidekiq::Worker
+  include Sidekiq::Worker
   
-    sidekiq_options retry: false
+  sidekiq_options retry: false
   
-    def perform
-      logger.info "Clearing Rails cache"
-      Rails.cache.clear
-      logger.info "Clearing Rails cache completed"
-    end
+  def perform
+    logger.info "Clearing Rails cache"
+    Rails.cache.clear
+    logger.info "Clearing Rails cache completed"
   end
 end
