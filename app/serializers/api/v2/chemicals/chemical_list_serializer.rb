f2ca4af2fee734190ec177bfebcfc8ed8b4873e5
Christopher Unger <mcu@mac.com>
2020-04-10 09:50:52 -0400

    Response to code review: - add app/serializers/api/v2/chemicals/chemical_name_serializer.rb - add newlines to the end of two files

--- app/serializers/api/v2/chemicals/chemical_list_serializer.rb
+++ app/serializers/api/v2/chemicals/chemical_list_serializer.rb
@@ -12,7 +12,7 @@ module Api
         attribute :name, &:name
 
         has_many :goods_nomenclatures, serializer: Api::V2::GoodsNomenclatures::GoodsNomenclatureListSerializer
-        has_many :chemical_names#, serializer: Api::V2::GoodsNomenclatures::GoodsNomenclatureListSerializer
+        has_many :chemical_names, serializer: Api::V2::Chemicals::ChemicalNameSerializer
       end
     end
   end
