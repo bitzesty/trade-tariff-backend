ab1d8aa5fea7570550749cbf02b1814c9495e0b5
Christopher Unger <mcu@mac.com>
2020-05-21 16:14:09 -0400

    Update searches to allow "CAS" prefix - update `SearchService` - update `ChemicalSearchService`
    New search behavior for the `:cas` param:
    - may be just the CAS number alone, e.g. `10310-21-1`
    - optional leading 'cas', with or without spaces after, e.g. `cas 10310-21-1`
    - optional other text between the leading 'cas' and the CAS number. e.g. `cas rn 10310-21-1`
    - optional other text before and/or after the CAS number. e.g. `cas rn blah 10310-21-1foobar biz baz   other text`
    - additional digits after the CAS number are ignored. Note: CAS numbers always end in a dash, then a single digit (`-\d{1}`) e.g. `10310-21-1684984654687` is interpreted as `10310-21-1`

--- app/services/chemical_search_service.rb
+++ app/services/chemical_search_service.rb
@@ -17,7 +17,7 @@ class ChemicalSearchService
   private
 
   def fetch_by_cas
-    return unless cas
+    return unless cas = cas_cleaned
 
     @chemicals = Rails.cache.fetch(cache_id, expires_in: cache_expiry) do
       Chemical.where(Sequel.like(:cas, "%#{cas}%")).all
@@ -50,10 +50,24 @@ class ChemicalSearchService
   end
 
   def cache_id
-    "chemical-search-#{(cas.presence || name.presence)}"
+    "chemical-search-#{(cas_cleaned.presence || name.presence)}"
   end
 
   def cache_expiry(seconds = nil)
     seconds || TradeTariffBackend.seconds_till_6am
+  end
+
+  def cas_cleaned
+    return unless @cas
+
+    if m = /\A(cas\s*.*?\s*)?(\d+-\d+-\d{1}).*\z/i.match(term)
+      # ^ Extract the CAS number from a string
+      # - may be just the CAS number alone, e.g. `10310-21-1`
+      # - optional leading 'cas', with or without spaces after, e.g. `cas 10310-21-1`
+      # - optional other text between the leading 'cas' and the CAS number. e.g. `cas rn 10310-21-1`
+      # - optional other text before and/or after the CAS number. e.g. `cas rn blah 10310-21-1foobar biz baz   other text`
+      # - additional digits after the CAS number are ignored. Note: CAS numbers always end in a dash, then a single digit (`-\d{1}`) e.g. `10310-21-1684984654687` is interpreted as `10310-21-1`
+      m[2]
+    end
   end
 end
--- app/services/search_service.rb
+++ app/services/search_service.rb
@@ -57,8 +57,13 @@ class SearchService
   def q=(term)
     # if search term has no letters extract the digits
     # and perform search with just the digits
-    @q = if m = /\A(cas\s*)?(\d+-\d+-\d)\z/i.match(term)
-           # handle CAS search, with optional leading string "cas "
+    @q = if m = /\A(cas\s*.*?\s*)?(\d+-\d+-\d{1}).*\z/i.match(term)
+          # ^ Extract the CAS number from the search term
+          # - may be just the CAS number alone, e.g. `10310-21-1`
+          # - optional leading 'cas', with or without spaces after, e.g. `cas 10310-21-1`
+          # - optional other text between the leading 'cas' and the CAS number. e.g. `cas rn 10310-21-1`
+          # - optional other text before and/or after the CAS number. e.g. `cas rn blah 10310-21-1foobar biz baz   other text`
+          # - additional digits after the CAS number are ignored. Note: CAS numbers always end in a dash, then a single digit (`-\d{1}`) e.g. `10310-21-1684984654687` is interpreted as `10310-21-1`
            m[2]
          elsif /^(?!.*[A-Za-z]+).*$/.match?(term)
            term.scan(/\d+/).join
