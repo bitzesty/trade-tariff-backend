55336e232a447c6828dc7abdb15c8c2425fb3889
Alexey Klimuk <alexey.klimuk@bitzesty.com>
2020-09-08 22:05:26 +0200

    Fix forum link specs

--- app/serializers/api/v2/headings/chapter_serializer.rb
+++ app/serializers/api/v2/headings/chapter_serializer.rb
@@ -9,7 +9,11 @@ module Api
         set_id :goods_nomenclature_sid
 
         attributes :goods_nomenclature_item_id, :description, :formatted_description,
-                   :chapter_note, :forum_url
+                   :chapter_note
+
+        attribute :forum_url do |chapter|
+          chapter.forum_link&.url
+        end
 
         has_many :guides, serializer: Api::V2::GuideSerializer
       end
