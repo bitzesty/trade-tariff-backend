4dcf1bb8b04f2eb5dfa880c81170b23718471136
Aleksey Zolotov <myxrome@gmail.com>
2020-09-22 10:34:59 +0400

    changed footnote - commodity association remove script to enddate association, not to delete.

--- db/data_migrations/20200921120101_delete_05004_footnote.rb
+++ db/data_migrations/20200921120101_delete_05004_footnote.rb
@@ -1,50 +1,48 @@
 TradeTariffBackend::DataMigrator.migration do
-  name 'Delete 05044 footnote for 7304293080 commodity'
+  name 'End 05044 footnote for 7304293080 commodity'
+
+  FOOTNOTE_ID = '004'.freeze
+  FOOTNOTE_TYPE = '05'.freeze
+  GOODS_NOMENCLATURE_ITEM_ID = '7304293080'.freeze
+  VALIDITY_END_DATE = Date.new(2020, 9, 20)
 
   up do
     applicable do
-      FootnoteAssociationGoodsNomenclature::Operation.where(
-        goods_nomenclature_sid: 86621,
-        footnote_id: '004',
-        footnote_type: '05',
-        goods_nomenclature_item_id: '7304293080',
-        operation: 'D'
-      ).none?
+      FootnoteAssociationGoodsNomenclature.where(
+        footnote_id: FOOTNOTE_ID,
+        footnote_type: FOOTNOTE_TYPE,
+        goods_nomenclature_item_id: GOODS_NOMENCLATURE_ITEM_ID
+      ).where('validity_end_date <> ? OR validity_end_date IS NULL', VALIDITY_END_DATE).any?
     end
 
     apply do
-      FootnoteAssociationGoodsNomenclature::Operation.create(
-        goods_nomenclature_sid: 86621,
-        footnote_id: '004',
-        footnote_type: '05',
-        goods_nomenclature_item_id: '7304293080',
-        productline_suffix: '80',
-        national: true,
-        validity_start_date: '2020-09-20 00:00:00.000000',
-        validity_end_date: nil,
-        operation: 'D'
-      )
+      FootnoteAssociationGoodsNomenclature.where(
+        footnote_id: FOOTNOTE_ID,
+        footnote_type: FOOTNOTE_TYPE,
+        goods_nomenclature_item_id: GOODS_NOMENCLATURE_ITEM_ID
+      ).where('validity_end_date <> ? OR validity_end_date IS NULL', VALIDITY_END_DATE).each do |association|
+        association.update(validity_end_date: VALIDITY_END_DATE)
+      end
     end
   end
 
   down do
     applicable do
-      FootnoteAssociationGoodsNomenclature::Operation.where(
-        goods_nomenclature_sid: 86621,
-        footnote_id: '004',
-        footnote_type: '05',
-        goods_nomenclature_item_id: '7304293080',
-        operation: 'D'
+      FootnoteAssociationGoodsNomenclature.where(
+        footnote_id: FOOTNOTE_ID,
+        footnote_type: FOOTNOTE_TYPE,
+        goods_nomenclature_item_id: GOODS_NOMENCLATURE_ITEM_ID,
+        validity_end_date: VALIDITY_END_DATE
       ).any?
     end
 
     apply do
       FootnoteAssociationGoodsNomenclature::Operation.where(
-        goods_nomenclature_sid: 86621,
-        footnote_id: '004',
-        footnote_type: '05',
-        goods_nomenclature_item_id: '7304293080',
-        operation: 'D'
+        footnote_id: FOOTNOTE_ID,
+        footnote_type: FOOTNOTE_TYPE,
+        goods_nomenclature_item_id: GOODS_NOMENCLATURE_ITEM_ID,
+        validity_end_date: VALIDITY_END_DATE,
+        operation: 'U'
       ).delete
     end
   end
