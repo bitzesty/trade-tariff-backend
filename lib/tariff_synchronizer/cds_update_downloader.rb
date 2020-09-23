390362f4002da9048412d82b3d72c0d02a77158f
Alexey Klimuk <alexey.klimuk@bitzesty.com>
2020-09-23 20:43:13 +0200

    [cds] Add authorization call

--- lib/tariff_synchronizer/cds_update_downloader.rb
+++ lib/tariff_synchronizer/cds_update_downloader.rb
@@ -1,6 +1,8 @@
 module TariffSynchronizer
-  # Download pending updates CDS files
   class CdsUpdateDownloader
+
+    class AuthorisationError < StandardError; end
+
     delegate :instrument, :subscribe, to: ActiveSupport::Notifications
 
     attr_reader :date
@@ -35,14 +37,13 @@ module TariffSynchronizer
 
     def response
       @response ||= begin
-        uri = URI(ENV['CDS_SYNC_HOST'])
+        uri = URI::join(ENV['HMRC_API_HOST'], '/bulk-data-download/list/TARIFF-DAILY')
         https = Net::HTTP.new(uri.host, uri.port)
         https.use_ssl = true
         request = Net::HTTP::Get.new(uri.request_uri)
         request["User-Agent"] = "Trade Tariff Backend"
         request["Accept"] = "application/vnd.hmrc.1.0+json"
-        request["Content-Type"] = "application/json"
-        request["Authorization"] = "Bearer #{ENV['CDS_SYNC_TOKEN']}"
+        request["Authorization"] = "Bearer #{access_token}"
         https.request(request)
       end
     end
@@ -69,6 +70,26 @@ module TariffSynchronizer
 
       update_or_create(BaseUpdate::MISSING_STATE, missing_filename)
       instrument("cds_not_found.tariff_synchronizer", date: date)
+    end
+
+    def access_token
+      uri = URI::join(ENV['HMRC_API_HOST'], '/oauth/token')
+      https = Net::HTTP.new(uri.host, uri.port)
+      https.use_ssl = true
+      request = Net::HTTP::Post.new(uri.request_uri)
+      request.set_form_data(
+        client_id: ENV['HMRC_CLIENT_ID'],
+        client_secret: ENV['HMRC_CLIENT_SECRET'],
+        grant_type: 'client_credentials'
+      )
+
+      response = https.request(request)
+
+      if response.code == '200'
+        JSON.parse(response.body)['access_token']
+      else
+        raise AuthorisationError.new(response.body)
+      end
     end
   end
 end
