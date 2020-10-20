3a7eafbdb2021dfcd72a337fec6968435c2cc107
Alexey Klimuk <alexey.klimuk@bitzesty.com>
2020-10-20 14:11:49 +0200

    Fix data migration for assigning footnote 05853 to commodities 21039090 81/89

--- db/data_migrations/20201008154642_add_footnote_to_210390908189.rb
+++ db/data_migrations/20201008154642_add_footnote_to_210390908189.rb
@@ -4,28 +4,33 @@ TradeTariffBackend::DataMigrator.migration do
   up do
     applicable { true }
     apply {
+      TimeMachine.at("2020-10-08") do
+        footnote = Footnote.where(footnote_id: "853", footnote_type_id: "05").first
 
-      footnote = Footnote.where(footnote_id: "853", footnote_type_id: "05").first
+        codes = %w[2103909081 2103909089]
 
-      codes = %w[2103909081 2103909089]
-
-      codes.each do |code|
-        Sequel::Model.db.fetch("SELECT goods_nomenclature_item_id FROM measures_oplog
+        codes.each do |code|
+          Sequel::Model.db.fetch("SELECT goods_nomenclature_item_id FROM measures_oplog
                                 WHERE goods_nomenclature_item_id LIKE '#{code}%'
                                 GROUP BY goods_nomenclature_item_id").all do |measure|
-          puts "Running code #{code} and Measure with gono_item_id #{measure[:goods_nomenclature_item_id]}"
-          goods_nomenclature = GoodsNomenclature.where(goods_nomenclature_item_id: measure[:goods_nomenclature_item_id]).first
+            puts "Running code #{code} and Measure with gono_item_id #{measure[:goods_nomenclature_item_id]}"
 
-          if goods_nomenclature.nil?
-            puts "GoodsNomenclature #{measure.inspect} does not exist"
-            next
-          end
-          if goods_nomenclature.footnotes_dataset.where(footnotes__footnote_id: footnote.footnote_id,
-                                                        footnotes__footnote_type_id: footnote.footnote_type_id).empty?
-            puts "Creating association for Footnote #{footnote.inspect} and GoodsNomenclature #{goods_nomenclature.inspect}"
-            FootnoteAssociationGoodsNomenclature.associate_footnote_with_goods_nomenclature(goods_nomenclature, footnote)
-          else
-            puts "Footnote is already associated with GoodsNomenclature #{measure.inspect}"
+            goods_nomenclature = GoodsNomenclature.actual.declarable.where(
+              goods_nomenclature_item_id: measure[:goods_nomenclature_item_id]
+            ).first
+
+            if goods_nomenclature.nil?
+              puts "GoodsNomenclature #{measure.inspect} does not exist"
+              next
+            end
+
+            if goods_nomenclature.footnotes_dataset.where(footnotes__footnote_id: footnote.footnote_id,
+                                                          footnotes__footnote_type_id: footnote.footnote_type_id).empty?
+              puts "Creating association for Footnote #{footnote.inspect} and GoodsNomenclature #{goods_nomenclature.inspect}"
+              FootnoteAssociationGoodsNomenclature.associate_footnote_with_goods_nomenclature(goods_nomenclature, footnote)
+            else
+              puts "Footnote is already associated with GoodsNomenclature #{measure.inspect}"
+            end
           end
         end
       end
@@ -33,11 +38,7 @@ TradeTariffBackend::DataMigrator.migration do
   end
 
   down do
-    applicable {
-      false
-    }
-    apply {
-      # noop
-    }
+    applicable { false }
+    apply {} # noop
   end
 end
