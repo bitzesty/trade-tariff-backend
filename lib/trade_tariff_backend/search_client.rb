620c1b77ed0577b1c79aefaaf48d07061ac01f19
Alexey Klimuk <alexey.klimuk@bitzesty.com>
2020-10-23 00:13:29 +0200

    Fix search client response to be compatible with serializer gem

--- lib/trade_tariff_backend/search_client.rb
+++ lib/trade_tariff_backend/search_client.rb
@@ -7,6 +7,12 @@ module TradeTariffBackend
 
     class Response < Hashie::Mash
       disable_warnings
+
+      # Need to wrap object in array because serializer gem does Array(obj) and it breaks Hashie::Mash object
+      # See changes https://github.com/jsonapi-serializer/jsonapi-serializer/commit/f62a5bf1622fd2da0278e2fef0e8d4342b97e7cc
+      def to_a
+        Array.wrap(self)
+      end
     end
 
     attr_reader :indexed_models
