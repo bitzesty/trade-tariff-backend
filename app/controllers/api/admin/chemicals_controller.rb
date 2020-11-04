9306a0d399d895415b6746c6426aa22af9f916ac
Christopher Unger <christopher.unger@bitzesty.com>
2020-11-04 01:00:58 -0500

    Refactor controller

--- app/controllers/api/admin/chemicals_controller.rb
+++ app/controllers/api/admin/chemicals_controller.rb
@@ -2,14 +2,12 @@ module Api
   module Admin
     class ChemicalsController < ApiController
       before_action :authenticate_user!
+      before_action :set_up_errors
       before_action :fetch_chemical, only: %i[show show_map]
       before_action :fetch_objects, only: %i[create_map update_map]
-      before_action :set_up_errors
 
       # GET    /admin/chemicals
       def index
-        # @errors = []
-
         respond_with Chemical.order_by(:id).all
       end
 
@@ -29,45 +27,42 @@ module Api
       def create_map
         if @map.present?
           @errors << "Mapping already exists: chemical_id: #{@chemical.id}, goods_nomenclature_sid: #{@commodity.id}"
-          respond_with(@chemical.refresh) and return
+          respond_with(@chemical.refresh, status: :conflict) and return
         end
 
+        status = :created
         if @chemical.present? && @commodity.present?
           create_chemical_commodity_mapping
           fetch_map
           unless @map.present?
-            @errors << "Newly created mapping was not found: chemical_id: #{@chemical.id}, goods_nomenclature_sid: #{@commodity.id}"
+            @errors << "Newly created mapping was not found: chemical_id: #{@chemical&.id}, goods_nomenclature_sid: #{@commodity&.id}"
+            status = :internal_server_error
           end
         else
-          @errors << "Target commodity and/or chemical missing: chemical.id: #{@chemical.id}, goods_nomenclature_sid: #{@commodity.goods_nomenclature_item_id}"
+          @errors << "Target commodity and/or chemical missing: chemical.id: #{@chemical&.id}, goods_nomenclature_sid: #{@commodity&.id}"
+          status = :not_found
         end
 
-        respond_with @chemical.refresh
+        respond_with @chemical.refresh, status: status
       end
 
       # PATCH /admin/chemicals/:chemical_id/map/:gn_id
       # PUT   /admin/chemicals/:chemical_id/map/:gn_id
       def update_map
+        status = :accepted
         if @chemical.present? && @map.present? && @new_commodity.present?
-          update_chemical_commodity_mapping
+          status = update_chemical_commodity_mapping
         else
           @errors << "Mapping was not updated: chemical.id: #{@chemical&.id}, old_gn: #{@map&.goods_nomenclature_sid}, new_gn: #{@new_commodity&.goods_nomenclature_item_id}"
+          status = :not_found
         end
 
-        respond_with @chemical.refresh
+        respond_with @chemical.refresh, status: status
       end
-      alias update update_map
 
       private
 
-      def chemical_params
-        # params.require(:data).permit(:type, attributes: [:title])
-        params.require(:data).permit!
-      end
-
       def fetch_chemical
-        # @errors = []
-
         unless id = params[:chemical_id] || params[:id] || false
           @errors << "Chemical id not found: chemical_id: #{id}"
         end
@@ -86,13 +81,16 @@ module Api
         @map = ChemicalsGoodsNomenclatures.find(
           chemical_id: @chemical.id,
           goods_nomenclature_sid: @commodity.id
-        )
+        ) unless @errors.any?
       end
       
       def fetch_commodity
-        @commodity = GoodsNomenclature.where(goods_nomenclature_sid: params[:gn_id]).first
-      rescue PG::NumericValueOutOfRange
-        GoodsNomenclature.where(goods_nomenclature_item_id: params[:gn_id]).first
+        @commodity = begin
+          GoodsNomenclature.where(goods_nomenclature_sid: params[:gn_id]).first
+        rescue Sequel::DatabaseError#PG::NumericValueOutOfRange
+          GoodsNomenclature.where(goods_nomenclature_item_id: params[:gn_id]).first
+        end
+        @errors << "Commodity not found: id: #{params[:gn_id]}" unless @commodity
       end
 
       def fetch_new_commodity
@@ -100,10 +98,10 @@ module Api
       end
 
       def set_up_errors
-        @errors = []
+        @errors ||= []
       end
 
-      def respond_with(obj, errors = @errors)
+      def respond_with(obj, status: :ok, errors: @errors)
         if errors.any?
           data = { errors: [] }
           data[:errors] = errors.map do |error|
@@ -111,7 +109,7 @@ module Api
           end
           render json: data, status: :unprocessable_entity
         else
-          render json: Api::Admin::Chemicals::ChemicalSerializer.new(obj || @chemical.refresh).serializable_hash
+          render json: Api::Admin::Chemicals::ChemicalSerializer.new(obj || @chemical.refresh).serializable_hash, status: status
         end
       end
 
@@ -132,9 +130,12 @@ module Api
             goods_nomenclature_sid: @new_commodity.id
           )
           @map.destroy
+          status = :accepted
         rescue StandardError
           @errors << "Mapping already exists: chemical_id: #{@chemical.id}, goods_nomenclature_sid: #{@commodity.id}"
+          status = :conflict
         end
+        status
       end
     end
   end
