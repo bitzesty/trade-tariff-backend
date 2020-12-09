9253993a1786bdf3db2d5e9b7bdd70c5f91cb7c1
Christopher Unger <christopher.unger@bitzesty.com>
2020-12-09 15:17:32 -0500

    Make Chemical auditable - add auditable plugin to Chemical model - add spec

--- app/models/chemical.rb
+++ app/models/chemical.rb
@@ -1,4 +1,6 @@
 class Chemical < Sequel::Model
+  plugin :auditable
+
   many_to_many :goods_nomenclatures, join_table: :chemicals_goods_nomenclatures, right_key: :goods_nomenclature_sid, left_key: :chemical_id
   one_to_many :chemical_names
 
--- spec/unit/sequel/plugins/auditable_spec.rb
+++ spec/unit/sequel/plugins/auditable_spec.rb
@@ -18,4 +18,23 @@ describe "Auditable sequel plugin" do
       expect(result["content"][1]).to eq("second content")
     end
   end
+
+  describe 'Chemical model is auditable' do
+    let(:cas0)      { '12-34-56' }
+    let(:cas1)      { '34-56-12' }
+    let(:cas2)      { '56-12-34' }
+    let(:chemical)  { create :chemical, cas: cas0 }
+
+    it 'creates an audit record when chemical updated' do
+      expect { chemical.update(cas: cas1) }.to change { Audit.count }.by(1)
+    end
+
+    it 'the new audit record created keeps the record of the changes to chemical' do
+      chemical.update(cas: cas2)
+      result = JSON.parse(chemical.audits.last.changes)
+
+      expect(result['cas'][0]).to eq(cas0)
+      expect(result['cas'][1]).to eq(cas2)
+    end
+  end
 end
